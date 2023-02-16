3b1d59881db018bda4e3b678516be881ae729b1c
Sindre Sægrov <sindre.segrov@nav.no>
2023-02-16 13:09:49 +0100

    Fjern test dependency

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -11,7 +11,7 @@ export async function loader() {
 
 export default function Index() {
   const oppgaver = useLoaderData<typeof loader>();
-
+  console.log("test");
   return (
     <div>
       <Header>
