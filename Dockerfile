96279faf4008ac84793e6ff3b76a8240adb3f9af
Sindre Sægrov <sindre.segrov@nav.no>
2023-02-17 14:14:21 +0100

    yolo

--- Dockerfile
+++ Dockerfile
@@ -5,14 +5,12 @@ ENV PORT=3000 \
     NODE_ENV=production \
     TZ=Europe/Oslo
 
-COPY remix.config.js ./
-COPY package.json ./
-
 COPY public ./public
 COPY build ./build
 COPY node_modules ./node_modules
+COPY server.js ./server.js
 
 EXPOSE 3000
 USER node
 
-CMD ["npm", "run" ,"start"]
+CMD ["node", "server.js"]
---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const { createApp } = require("@remix-run/serve");
+const { serverBuildDirectory } = require("./remix.config");
+
+const buildPath = path.resolve(process.cwd(), serverBuildDirectory);
+
+const app = createApp(buildPath);
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  // eslint-disable-next-line
+  console.log(`Remix App Server started at http://localhost:${port}`);
+});
