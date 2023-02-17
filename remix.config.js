6e7f4ab2303b6ff79b012af0eff2b8858894a161
Sindre Sægrov <sindre.segrov@nav.no>
2023-02-17 14:27:35 +0100

    noch einmal

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+  assetsBuildDirectory: "public/build",
+  serverBuildPath: "build/index.js",
+  publicPath: "/build/",
   future: {
     unstable_cssModules: true,
     v2_routeConvention: true,
