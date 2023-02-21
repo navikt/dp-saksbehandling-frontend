78094c9a4c70583af88b4fb6f5562bcbd3a44c19
Knut Magne Riise <knut.magne.riise@nav.no>
2023-02-21 13:43:39 +0100

    sette basepath i publicpath configen også for å hente statiske filer riktig

--- remix.config.js
+++ remix.config.js
@@ -1,10 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
+const basePath = "/saksbehandling";
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   assetsBuildDirectory: "public/build",
   serverBuildPath: "build/index.js",
-  publicPath: "/build/",
+  publicPath: `${basePath}/build/`,
   future: {
     unstable_cssModules: true,
     //v2_routeConvention: true, fjerner denne enn så lenge da den bare gir et blankt ark
