d36bc029577d940aae030e23bb328617ada1ff0b
Knut Magne Riise <knut.magne.riise@nav.no>
2023-02-21 10:44:57 +0100

    prettier on save

--- server/server.ts
+++ server/server.ts
@@ -10,33 +10,33 @@ const basePath = "";
 const app = express();
 
 const configureServerSettings = () => {
-    // Gzip responser
-    app.use(compression());
+  // Gzip responser
+  app.use(compression());
 
-    // Sikkerhetsgreier
-    app.disable("x-powered-by");
+  // Sikkerhetsgreier
+  app.disable("x-powered-by");
 
-    // Remix bygger appen med fingerprinting, så vi kan cache disse filene evig
-    app.use(`${basePath}/build`, express.static("public/build", { immutable: true, maxAge: "1y" }));
+  // Remix bygger appen med fingerprinting, så vi kan cache disse filene evig
+  app.use(`${basePath}/build`, express.static("public/build", { immutable: true, maxAge: "1y" }));
 
-    // Cache public-filer (som favicon) i én time
-    app.use(express.static("public", { maxAge: "1h" }));
+  // Cache public-filer (som favicon) i én time
+  app.use(express.static("public", { maxAge: "1h" }));
 };
 
 const startServer = async () => {
-    configureServerSettings();
+  configureServerSettings();
 
-    app.get([`${basePath}/internal/isAlive`, `${basePath}/internal/isReady`], (_, res: Response) =>
-        res.sendStatus(200)
-    );
+  app.get([`${basePath}/internal/isAlive`, `${basePath}/internal/isReady`], (_, res: Response) =>
+    res.sendStatus(200)
+  );
 
-    app.use(logRequests);
+  app.use(logRequests);
 
-    app.all("*", handleRequestWithRemix);
+  app.all("*", handleRequestWithRemix);
 
-    app.listen(port, () => {
-        logger.info(`Server kjører på port ${port}`);
-    });
+  app.listen(port, () => {
+    logger.info(`Server kjører på port ${port}`);
+  });
 };
 
 startServer();
