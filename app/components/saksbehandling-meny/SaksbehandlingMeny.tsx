14cbaa532b449c9a82ef5f57148bbdbcbdc8d490
Sindre Sægrov <sindre.segrov@nav.no>
2023-02-23 10:24:49 +0100

    Relativ routing

--- app/components/saksbehandling-meny/SaksbehandlingMeny.tsx
+++ app/components/saksbehandling-meny/SaksbehandlingMeny.tsx
@@ -1,22 +1,17 @@
 import React from "react";
 import styles from "./SaksbehandlingMeny.module.css";
 import { Link, useLocation } from "@remix-run/react";
-import type { IBruker } from "~/models/bruker.server";
 
-interface IProps {
-  bruker: IBruker;
-}
-
-export function SaksbehandlingMeny({ bruker }: IProps) {
+export function SaksbehandlingMeny() {
   let location = useLocation();
 
   console.log(location);
   return (
     <div className={styles.container}>
-      <Link to={`/saksbehandling/bruker/${bruker.ident}/vedtak`} className={styles.linkItem}>
+      <Link to={`vedtak`} className={styles.linkItem}>
         Vedtak
       </Link>
-      <Link to={`/saksbehandling/bruker/${bruker.ident}/meldekort`} className={styles.linkItem}>
+      <Link to={`meldekort`} className={styles.linkItem}>
         Meldekort
       </Link>
     </div>
