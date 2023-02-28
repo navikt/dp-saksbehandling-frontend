ec0fef816b66934dbca30c361008d84a14033a16
Sindre Sægrov <sindre.segrov@nav.no>
2023-02-28 07:49:41 +0100

    Legg til favicon

--- app/root.tsx
+++ app/root.tsx
@@ -15,9 +15,15 @@ export function links() {
   return [
     ...(cssBundleHref
       ? [
+          { rel: "stylesheet", href: cssBundleHref },
           { rel: "stylesheet", href: navStyles },
           { rel: "stylesheet", href: navInternalStyles },
-          { rel: "stylesheet", href: cssBundleHref },
+          { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+          { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
+          { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
+          { rel: "manifest", href: "/site.webmanifest" },
+          { rel: "manifest", href: "/site.webmanifest" },
+          { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
         ]
       : []),
   ];
