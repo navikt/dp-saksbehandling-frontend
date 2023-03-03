5d01e4ac7ce9c124dbd28a544b3b1fe999f23e69
Sindre Sægrov <sindre.segrov@nav.no>
2023-03-03 12:21:28 +0100

    Mock data

--- app/components/dagpenger-tidslinje/DagpengerTidslinje.tsx
+++ app/components/dagpenger-tidslinje/DagpengerTidslinje.tsx
@@ -1,109 +1,105 @@
-import { Coronavirus, Edit, Hospital, Money, People, Vacation } from "@navikt/ds-icons";
+import { BagFilled, EditFilled, People, SuccessFilled } from "@navikt/ds-icons";
+import type { TimelinePeriodProps } from "@navikt/ds-react-internal";
 import { Timeline } from "@navikt/ds-react-internal";
 
 export function DagpengerTidslinje() {
   return (
-    <div className="min-w-[800px] overflow-x-auto">
-      <Timeline>
-        <Timeline.Row label="Person" icon={<People aria-hidden />}>
-          {person.map((p: any, i) => {
-            return (
-              <Timeline.Period key={i} start={p.start} end={p.end} status={p.status} icon={p.icon}>
-                {p?.children ?? null}
-              </Timeline.Period>
-            );
-          })}
-        </Timeline.Row>
-        <Timeline.Row label="Sykehus A" icon={<Hospital aria-hidden />}>
-          {jobb.map((p: any, i) => {
-            return (
-              <Timeline.Period key={i} start={p.start} end={p.end} status={p.status} icon={p.icon}>
-                {p?.children ?? null}
-              </Timeline.Period>
-            );
-          })}
-        </Timeline.Row>
-        <Timeline.Row label="Sykehus B" icon={<Hospital aria-hidden />}>
-          {jobb.map((p: any, i) => {
-            return (
-              <Timeline.Period
-                key={i}
-                start={p.start}
-                end={p.end}
-                status={p.status}
-                icon={p.icon}
-              />
-            );
-          })}
-        </Timeline.Row>
-      </Timeline>
-    </div>
+    <Timeline>
+      <Timeline.Row label="ARBEIDSØKER" icon={<People aria-hidden />}>
+        <Timeline.Period
+          start={arbeidssokerPeriode.start}
+          end={arbeidssokerPeriode.end}
+          status={arbeidssokerPeriode.status}
+          icon={arbeidssokerPeriode.icon}
+        />
+      </Timeline.Row>
+
+      <Timeline.Row label="Dagpengeperiode (Sak)" icon={<People aria-hidden />}>
+        <Timeline.Period
+          start={dagpengerPeriode.start}
+          end={dagpengerPeriode.end}
+          status={dagpengerPeriode.status}
+          icon={dagpengerPeriode.icon}
+        />
+      </Timeline.Row>
+      <Timeline.Row label="Rapportering/utbetaling" icon={<People aria-hidden />}>
+        {rapportering.map((periode, index) => (
+          <Timeline.Period
+            key={index}
+            start={periode.start}
+            end={periode.end}
+            status={periode.status}
+            icon={periode.icon}
+          />
+        ))}
+      </Timeline.Row>
+    </Timeline>
   );
 }
 
-const person = [
+const arbeidssokerPeriode: TimelinePeriodProps = {
+  start: new Date("Jan 1 2022"),
+  end: new Date("May 1 2022"),
+  status: "info",
+  icon: <BagFilled aria-hidden />,
+  // statusLabel: "Sykemeldt",
+  // children: <div>50% sykemeldt</div>,
+};
+
+const dagpengerPeriode: TimelinePeriodProps = {
+  start: new Date("Jan 1 2022"),
+  end: new Date("May 1 2022"),
+  status: "warning",
+  icon: <EditFilled aria-hidden />,
+};
+
+const rapportering: TimelinePeriodProps[] = [
   {
     start: new Date("Jan 1 2022"),
+    end: new Date("Jan 14 2022"),
+    status: "success",
+    icon: <SuccessFilled />,
+  },
+  {
+    start: new Date("Jan 15 2022"),
     end: new Date("Jan 31 2022"),
-    status: "warning",
-    icon: <Edit aria-hidden />,
-    statusLabel: "Sykemeldt",
-    children: <div>50% sykemeldt</div>,
+    status: "success",
+    icon: <SuccessFilled />,
   },
   {
-    start: new Date("Apr 1 2022"),
-    end: new Date("Apr 30 2022"),
-    status: "neutral",
-    icon: <Vacation aria-hidden />,
-    label: "test",
-    statusLabel: "Ferie",
+    start: new Date("Feb 1 2022"),
+    end: new Date("Feb 14 2022"),
+    status: "success",
+    icon: <SuccessFilled />,
   },
   {
-    start: new Date("Jul 1 2022"),
-    end: new Date("Jul 31 2022"),
-    status: "warning",
-    icon: <Edit aria-hidden />,
-    statusLabel: "Sykemeldt",
-  },
-  {
-    start: new Date("Aug 1 2022"),
-    end: new Date("Aug 30 2022"),
-    status: "warning",
-    icon: <Coronavirus aria-hidden />,
-    statusLabel: "Stønad korona",
-  },
-];
-
-const jobb = [
-  {
-    start: new Date("Feb 2 2022"),
+    start: new Date("Feb 15 2022"),
     end: new Date("Mar 1 2022"),
     status: "success",
-    statusLabel: "Utbetaling",
-    icon: <Money aria-hidden />,
-    children: <div>100% utbetaling</div>,
+    icon: <SuccessFilled />,
   },
   {
     start: new Date("Mar 2 2022"),
-    end: new Date("Apr 1 2022"),
+    end: new Date("Mar 16 2022"),
     status: "success",
-    statusLabel: "Utbetaling",
-    icon: <Money aria-hidden />,
-    children: <div>100% utbetaling</div>,
+    icon: <SuccessFilled />,
   },
   {
-    start: new Date("May 2 2022"),
-    end: new Date("June 1 2022"),
+    start: new Date("Mar 17 2022"),
+    end: new Date("Mar 31 2022"),
     status: "success",
-    statusLabel: "Utbetaling",
-    icon: <Money aria-hidden />,
-    children: <div>100% utbetaling</div>,
+    icon: <SuccessFilled />,
   },
   {
-    start: new Date("June 2 2022"),
-    end: new Date("July 1 2022"),
+    start: new Date("Apr 1 2022"),
+    end: new Date("Apr 14 2022"),
     status: "success",
-    statusLabel: "Utbetaling",
-    icon: <Money aria-hidden />,
+    icon: <SuccessFilled />,
+  },
+  {
+    start: new Date("Apr 15 2022"),
+    end: new Date("Apr 28 2022"),
+    status: "success",
+    icon: <SuccessFilled />,
   },
 ];
