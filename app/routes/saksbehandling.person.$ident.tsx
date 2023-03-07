42f63c901bf12f2e858499ccdd33d3bc5fdb00a9
Knut Magne Riise <knut.magne.riise@nav.no>
2023-03-07 14:06:14 +0100

    mulighet til å skjule tidslinje

--- app/routes/saksbehandling.person.$ident.tsx
+++ app/routes/saksbehandling.person.$ident.tsx
@@ -7,6 +7,7 @@ import styles from "~/route-styles/bruker.module.css";
 import { Brodsmuler } from "~/components/brodsmuler/Brodsmuler";
 import { DagpengerTidslinje } from "~/components/dagpenger-tidslinje/DagpengerTidslinje";
 import { BrukerMeny } from "~/components/bruker-meny/BrukerMeny";
+import { Accordion } from "@navikt/ds-react";
 
 export async function loader({ request }: LoaderArgs) {
   return await mockHentPerson();
@@ -19,9 +20,17 @@ export default function Person() {
     <main>
       <Brodsmuler bruker={bruker} />
 
-      <div className={styles.tidslinjeContainer}>
-        <DagpengerTidslinje />
-      </div>
+      <Accordion className={styles.tidslinjeContainer}>
+        <Accordion.Item defaultOpen={true}>
+          <Accordion.Header>
+            Status: Aktiv sak || Dagsats: 100kr (rediger) || Ventetid: 0 (rediger) || Gjenstående
+            kvote: 420 (rediger) || Sanksjon: null (legg til sanksjon) || Barnetillegg: 0
+          </Accordion.Header>
+          <Accordion.Content>
+            <DagpengerTidslinje />
+          </Accordion.Content>
+        </Accordion.Item>
+      </Accordion>
 
       <BrukerMeny />
 
