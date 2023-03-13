0782116c5ab3f12189830a523ba60e07d2845751
Sindre Sægrov <sindre.segrov@nav.no>
2023-03-13 10:24:25 +0100

    fix favicon path

--- app/root.tsx
+++ app/root.tsx
@@ -21,23 +21,23 @@ export function links() {
           {
             rel: "apple-touch-icon",
             sizes: "180x180",
-            href: "saksbehandling/apple-touch-icon.png",
+            href: "/apple-touch-icon.png",
           },
           {
             rel: "icon",
             type: "image/png",
             sizes: "32x32",
-            href: "saksbehandling/favicon-32x32.png",
+            href: "/favicon-32x32.png",
           },
           {
             rel: "icon",
             type: "image/png",
             sizes: "16x16",
-            href: "saksbehandling/favicon-16x16.png",
+            href: "/favicon-16x16.png",
           },
-          { rel: "manifest", href: "saksbehandling/site.webmanifest" },
-          { rel: "manifest", href: "saksbehandling/site.webmanifest" },
-          { rel: "mask-icon", href: "saksbehandling/safari-pinned-tab.svg", color: "#5bbad5" },
+          { rel: "manifest", href: "/site.webmanifest" },
+          { rel: "manifest", href: "/site.webmanifest" },
+          { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#5bbad5" },
         ]
       : []),
   ];
