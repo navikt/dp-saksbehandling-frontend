480cc3a375b9e4796b23a1e44edc7f8a308e6e2b
Sindre Sægrov <sindre.segrov@nav.no>
2023-03-14 08:14:36 +0100

    Legg til to-trinnskontroll route
    Co-authored-by: Knut Magne Riise <knut.magne.riise@nav.no>
    Co-authored-by: Nattaphong Klinjan <nattaphong.klinjan@nav.no>
    Co-authored-by: Hanne Olsen <hanne.eilertsen.olsen@nav.no>

---
+++ app/routes/saksbehandling.person.$ident.behandle.$hendelseId.to-trinnskontroll.tsx
@@ -0,0 +1,19 @@
+import { Button, Heading } from "@navikt/ds-react";
+import { Form } from "@remix-run/react";
+import styles from "~/route-styles/mangelbrev.module.css";
+
+export default function SendTilToTrinnskontroll() {
+  return (
+    <div className={styles.container}>
+      <Form>
+        <Heading size={"large"} level={"1"}>
+          To Trinnskontroll
+        </Heading>
+
+        <div className={styles.buttonContainer}>
+          <Button>Send til to trinnskontroll</Button>
+        </div>
+      </Form>
+    </div>
+  );
+}
