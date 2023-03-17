79507d4408bf07bd915cbbffd57c2c92ab8ef20e
Sindre Sægrov <sindre.segrov@nav.no>
2023-03-17 11:39:11 +0100

    PDF i iframe

--- app/components/pdf-leser/PDFLeser.tsx
+++ app/components/pdf-leser/PDFLeser.tsx
@@ -1,28 +1,16 @@
 import React, { useState } from "react";
-import { Document, Page, pdfjs } from "react-pdf";
-import type { PDFDocumentProxy } from "pdfjs-dist/types/src/display/api";
 import { Heading, Select } from "@navikt/ds-react";
+
 import styles from "./PDFLeser.module.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.min.js`;
-
-const options = {
-  cMapUrl: "cmaps/",
-  cMapPacked: true,
-  standardFontDataUrl: "standard_fonts/",
-};
-
 export function PDFLeser() {
-  const [file, setFile] = useState("./sample.pdf");
-  const [numPages, setNumPages] = useState<number | undefined>();
+  const [fileUrl, setFileUrl] = useState("/sample.pdf");
 
-  function onFileChange(event: React.ChangeEvent<HTMLSelectElement>) {
-    // @ts-ignore
-    setFile(event.target.files[0]);
-  }
+  console.log(fileUrl);
 
-  function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy) {
-    setNumPages(nextNumPages);
+  function setUrl(event: React.ChangeEvent<HTMLSelectElement>) {
+    console.log(event.currentTarget.value);
+    setFileUrl(event.currentTarget.value);
   }
 
   return (
@@ -30,24 +18,16 @@ export function PDFLeser() {
       <Heading size={"medium"} level={"3"}>
         Dokumenter
       </Heading>
+
       <div>
-        <Select className={styles.dropdown} label={"Velg fil"} onChange={onFileChange}>
-          <option value="./sample.pdf">sample.pdf</option>
-          <option value="./test.pdf">test.pdf</option>
+        <Select className={styles.dropdown} label={"Velg fil"} onChange={setUrl}>
+          <option value={"/sample.pdf"}>sample.pdf</option>
+          <option value={"/test.pdf"}>test.pdf</option>
         </Select>
+      </div>
 
-        {/*<div>*/}
-        {/*  <label htmlFor="file">Load from file:</label>{" "}*/}
-        {/*  <input onChange={onFileChange} type="file" />*/}
-        {/*</div>*/}
-
-        <div>
-          <Document file={file} onLoadSuccess={onDocumentLoadSuccess} options={options}>
-            {Array.from(new Array(numPages), (el, index) => (
-              <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-            ))}
-          </Document>
-        </div>
+      <div className={styles.iframeWrapper}>
+        <iframe title={"Pdf leser"} src={fileUrl} className={styles.iframe}></iframe>
       </div>
     </div>
   );
