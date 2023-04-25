cf1cd054431df76afc3e418a524fccbdbc6309ff
Rebecca Gjerstad <rebecca.soraya.gjerstad@nav.no>
2023-04-25 09:53:24 +0200

    WIP parprog

--- app/routes/saksbehandling.person.$ident.oppgave.$oppgaveId.vedtaksbrev.tsx
+++ app/routes/saksbehandling.person.$ident.oppgave.$oppgaveId.vedtaksbrev.tsx
@@ -1,8 +1,24 @@
 import { Button, Heading } from "@navikt/ds-react";
+import { ActionArgs } from "@remix-run/node";
 import { Form } from "@remix-run/react";
 import { ClientOnly } from "remix-utils";
+import invariant from "tiny-invariant";
 import QuillEditor from "~/components/quill-editor/QuillEditor.client";
+import { endreStatus } from "~/models/oppgave.server";
 import styles from "~/route-styles/vedtaksbrev.module.css";
+
+export async function action({ request, params }: ActionArgs) {
+  invariant(params.oppgaveId, `params.oppgaveId er påkrevd`);
+  const nyTilstand = "Innstilt";
+
+  const body = {
+    nyTilstand,
+  };
+
+  const response = await endreStatus(params.oppgaveId, body);
+
+  return { response };
+}
 
 export default function SendMangelbrev() {
   return (
