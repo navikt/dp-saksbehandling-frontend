c7767c6069211b19cf30ca57535189b30ab597d8
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-04-25 09:34:28 +0200

    Fjernet playwright

--- tests/page-nagivation.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from "@playwright/test";
-
-// SlowMo mode for local debug
-// test.use({ launchOptions: { slowMo: 1000 } });
-
-test("Test benken menu button", async ({ page, baseURL }) => {
-  await page.goto(baseURL + "/");
-  await page.getByTestId("benken-menu-button").click();
-});
