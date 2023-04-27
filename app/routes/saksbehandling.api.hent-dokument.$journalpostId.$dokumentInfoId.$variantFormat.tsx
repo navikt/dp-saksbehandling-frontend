dc65f11abace730f95422ad69893b379e4ae705c
Sindre Sægrov <sindre.segrov@nav.no>
2023-04-27 15:52:45 +0200

    prøv noe token greier

--- app/routes/saksbehandling.api.hent-dokument.$journalpostId.$dokumentInfoId.$variantFormat.tsx
+++ app/routes/saksbehandling.api.hent-dokument.$journalpostId.$dokumentInfoId.$variantFormat.tsx
@@ -1,13 +1,24 @@
 import type { LoaderArgs } from "@remix-run/node";
 import invariant from "tiny-invariant";
+import { getAzureSession } from "~/utils/auth.utils";
 
-export async function loader({ params }: LoaderArgs) {
+export async function loader({ params, request }: LoaderArgs) {
   invariant(params.journalpostId, `params.journalpostId er påkrevd`);
   invariant(params.dokumentInfoId, `params.dokumentInfoId er påkrevd`);
   invariant(params.variantFormat, `params.variantFormat er påkrevd`);
 
   const url = `https://saf-q1.dev.intern.nav.no/rest/hentdokument/${params.journalpostId}/${params.dokumentInfoId}/${params.variantFormat}`;
-  const response = await fetch(url);
+  console.log("Hent dokument fra URL: ", url);
+
+  const session = await getAzureSession(request);
+  const oboToken = await session.apiToken("api://saf-q1.dev.intern.nav.no");
+
+  console.log(oboToken);
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${oboToken}`,
+    },
+  });
 
   if (!response.ok) {
     throw new Response(`Feil ved kall til ${url}`, {
