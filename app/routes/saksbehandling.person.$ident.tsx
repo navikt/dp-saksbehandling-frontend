c7d59bfaaf7719b38760c5e97aa2bed4a4d101ea
Nattaphong Klinjan <110396878+nattaphongklinjan@users.noreply.github.com>
2023-05-03 11:26:52 +0200

    Refakturering av dokumenter og loader data (#12)

--- app/routes/saksbehandling.person.$ident.tsx
+++ app/routes/saksbehandling.person.$ident.tsx
@@ -3,12 +3,22 @@ import { Outlet, useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 import { Brodsmuler } from "~/components/brodsmuler/Brodsmuler";
 import { mockHentPerson } from "~/models/person.server";
+import { hentDokumentOversiktMetadata } from "~/models/SAF.server";
+import { getEnv } from "~/utils/env.utils";
 
-export async function loader({ params }: LoaderArgs) {
+export const shouldRevalidate = () => false;
+
+export async function loader({ params, request }: LoaderArgs) {
   invariant(params.ident, `params.ident er påkrevd`);
   const person = await mockHentPerson(params.ident);
 
-  return json({ person });
+  let dokumentOversiktMetadata: any[] = [];
+
+  if (getEnv("IS_LOCALHOST") !== "true") {
+    dokumentOversiktMetadata = await hentDokumentOversiktMetadata(request, params.ident);
+  }
+
+  return json({ person, dokumentOversiktMetadata });
 }
 
 export default function Person() {
