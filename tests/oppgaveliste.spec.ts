9480709c5d1e85a8254beac89e780b67b307e4ac
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-05-10 09:21:37 +0200

    Fikset playwright test for auto redirect

--- tests/oppgaveliste.spec.ts
+++ tests/oppgaveliste.spec.ts
@@ -22,5 +22,5 @@ test("Man kan gå videre til en oppgave ", async ({ page, baseURL }) => {
     .click();
 
   await expect(page).toHaveURL(`
-    ${baseURL}/person/${oppgave.person}/oppgave/${oppgave.uuid}`);
+    ${baseURL}/person/${oppgave.person}/oppgave/${oppgave.uuid}/steg/${oppgave.steg[0].uuid}`);
 });
