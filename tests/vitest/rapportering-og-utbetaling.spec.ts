3cfaa3cfa1fee7f44a82a968593c4c18a6225b67
Hanne Eilertsen Olsen <hanne.eilertsen.olsen@nav.no>
2023-08-02 16:02:35 +0200

    Lager test for feilende backend-endepunkt

--- tests/vitest/rapportering-og-utbetaling.spec.ts
+++ tests/vitest/rapportering-og-utbetaling.spec.ts
@@ -3,6 +3,7 @@ import { loader } from "app/routes/saksbehandling.person.$ident.oversikt.rapport
 import { mockSession } from "./helpers/auth-helper";
 import { server } from "../../mocks/server";
 import { mockRapporteringsperioder } from "mocks/api-routes/rapporteringsperiodeResponse";
+import { rest } from "msw";
 
 describe("Rapportering og utbetaling", () => {
   beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
@@ -15,10 +16,10 @@ describe("Rapportering og utbetaling", () => {
         request: new Request("http://test"),
         params: { ident: "1234" },
         context: {},
-      })
-    } catch(e) {
+      });
+    } catch (e) {
       const error = e as Response;
-      expect(error.status).toBe(500)
+      expect(error.status).toBe(500);
     }
   });
 
@@ -36,5 +37,31 @@ describe("Rapportering og utbetaling", () => {
     expect(mock.getAzureSession).toHaveBeenCalledTimes(1);
     expect(response.status).toBe(200);
     expect(data.rapporteringsperioder).toEqual(mockRapporteringsperioder);
+  });
+
+  test("loader skal feile hvis backend-kallet feiler", async () => {
+    server.use(
+      rest.post(`${process.env.DP_RAPPORTERING_URL}/rapporteringsperioder/sok`, (req, res, ctx) => {
+        return res(
+          ctx.status(500),
+          ctx.json({
+            errorMessage: `Server Error`,
+          }),
+        );
+      }),
+    );
+
+    mockSession();
+
+    try {
+      await loader({
+        request: new Request("http://test"),
+        params: { ident: "1234" },
+        context: {},
+      });
+    } catch (e) {
+      const error = e as Response;
+      expect(error.status).toBe(500);
+    }
   });
 });
