807d40beeea1a99571f26d10379628562f3038c0
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2023-09-08 17:29:12 +0200

    Setter skip istedenfor kommenter ut testing

--- tests/vitest/routes/root.api.spec.ts
+++ tests/vitest/routes/root.api.spec.ts
@@ -1,74 +1,74 @@
-// // @vitest-environment node
-// import { oppgaverResponse } from "mocks/api-routes/oppgaverResponse";
-// import { rest } from "msw";
-// import { afterAll, afterEach, beforeAll, describe, expect, test } from "vitest";
-// import { loader } from "~/root";
-// import { server } from "../../../mocks/server";
-// import { endSessionMock, mockSession } from "../helpers/auth-helper";
-// import { catchErrorResponse } from "../helpers/response-helper";
+// @vitest-environment node
+import { oppgaverResponse } from "mocks/api-routes/oppgaverResponse";
+import { rest } from "msw";
+import { afterAll, afterEach, beforeAll, describe, expect, test } from "vitest";
+import { loader } from "~/root";
+import { server } from "../../../mocks/server";
+import { endSessionMock, mockSession } from "../helpers/auth-helper";
+import { catchErrorResponse } from "../helpers/response-helper";
 
-// describe("Root til applikasjonen", () => {
-//   beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
-//   afterAll(() => server.close());
-//   afterEach(() => {
-//     server.resetHandlers();
-//     endSessionMock();
-//   });
+describe("Root til applikasjonen", () => {
+  beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+  afterAll(() => server.close());
+  afterEach(() => {
+    server.resetHandlers();
+    endSessionMock();
+  });
 
-//   describe("Loader", () => {
-//     const testParams = {};
+  describe("Loader", () => {
+    const testParams = {};
 
-//     test("skal feile hvis bruker ikke er logget på", async () => {
-//       const response = await catchErrorResponse(() =>
-//         loader({
-//           request: new Request("http://localhost:3000"),
-//           params: testParams,
-//           context: {},
-//         }),
-//       );
+    test("skal feile hvis bruker ikke er logget på", async () => {
+      const response = await catchErrorResponse(() =>
+        loader({
+          request: new Request("http://localhost:3000"),
+          params: testParams,
+          context: {},
+        }),
+      );
 
-//       expect(response.status).toBe(500);
-//     });
+      expect(response.status).toBe(500);
+    });
 
-//     test("skal feile hvis backend-kallet feiler", async () => {
-//       server.use(
-//         rest.get(`${process.env.DP_BEHANDLING_URL}/oppgave`, (req, res, ctx) => {
-//           return res.once(
-//             ctx.status(500),
-//             ctx.json({
-//               errorMessage: `Server Error`,
-//             }),
-//           );
-//         }),
-//       );
+    test.skip("skal feile hvis backend-kallet feiler", async () => {
+      server.use(
+        rest.get(`${process.env.DP_BEHANDLING_URL}/oppgave`, (req, res, ctx) => {
+          return res.once(
+            ctx.status(500),
+            ctx.json({
+              errorMessage: `Server Error`,
+            }),
+          );
+        }),
+      );
 
-//       mockSession();
+      mockSession();
 
-//       const response = await catchErrorResponse(() =>
-//         loader({
-//           request: new Request("http://localhost:3000"),
-//           params: testParams,
-//           context: {},
-//         }),
-//       );
+      const response = await catchErrorResponse(() =>
+        loader({
+          request: new Request("http://localhost:3000"),
+          params: testParams,
+          context: {},
+        }),
+      );
 
-//       expect(response.status).toBe(500);
-//     });
+      expect(response.status).toBe(500);
+    });
 
-//     test("skal hente ut alle oppgaver i saksbehandlingssystemet", async () => {
-//       const mock = mockSession();
+    test.skip("skal hente ut alle oppgaver i saksbehandlingssystemet", async () => {
+      const mock = mockSession();
 
-//       const response = await loader({
-//         request: new Request("http://localhost:3000"),
-//         params: testParams,
-//         context: {},
-//       });
+      const response = await loader({
+        request: new Request("http://localhost:3000"),
+        params: testParams,
+        context: {},
+      });
 
-//       const data = await response.json();
+      const data = await response.json();
 
-//       expect(mock.getAzureSession).toHaveBeenCalledTimes(1);
-//       expect(response.status).toBe(200);
-//       expect(data).toEqual(oppgaverResponse);
-//     });
-//   });
-// });
+      expect(mock.getAzureSession).toHaveBeenCalledTimes(1);
+      expect(response.status).toBe(200);
+      expect(data).toEqual(oppgaverResponse);
+    });
+  });
+});
