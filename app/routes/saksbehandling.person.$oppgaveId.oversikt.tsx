cf851aba999cd3b6eb8af52e1f2c47038223db2c
Rebecca Gjerstad <rebecca.soraya.gjerstad@nav.no>
2023-09-11 13:14:29 +0200

    NAV-15242 Lagt inn kall for stans og knapp

--- app/routes/saksbehandling.person.$oppgaveId.oversikt.tsx
+++ app/routes/saksbehandling.person.$oppgaveId.oversikt.tsx
@@ -1,13 +1,28 @@
-import { Outlet } from "@remix-run/react";
-import { DagpengerStatus } from "~/components/dagpenger-status/DagpengerStatus";
+import { Button } from "@navikt/ds-react";
+import { type ActionArgs, json } from "@remix-run/node";
+import { Form, Outlet } from "@remix-run/react";
+import invariant from "tiny-invariant";
 import { PersonOversiktTabs } from "~/components/person-oversikt-tabs/PersonOversiktTabs";
+import { stans } from "~/models/oppgave.server";
 
 import styles from "~/route-styles/person.module.css";
+
+export async function action({ request, params }: ActionArgs) {
+  invariant(params.oppgaveId, "OppgaveId må være satt");
+
+  const periodeId = params.oppgaveId as string;
+
+  const response = await stans(periodeId, request);
+
+  return json({ response });
+}
 
 export default function PersonOversikt() {
   return (
     <div className={styles.personOversiktKontainer}>
-      <DagpengerStatus />
+      <Form method="post">
+        <Button>Avbrudd</Button>
+      </Form>
       <PersonOversiktTabs />
       <Outlet />
     </div>
