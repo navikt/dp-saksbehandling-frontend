c138b3fc619e894c54082d34a1fd3570a564df18
Sindre Sægrov <sindre.segrov@nav.no>
2023-09-22 09:46:54 +0200

    Fjern unødvendig setup

--- tests/vitest/helpers/setup.ts
+++ tests/vitest/helpers/setup.ts
@@ -1,10 +1,6 @@
 import "@testing-library/jest-dom/vitest";
-import { afterEach, expect, vi } from "vitest";
+import { afterEach, vi } from "vitest";
 import { cleanup } from "@testing-library/react";
-import * as matchers from "@testing-library/jest-dom/matchers";
-
-// extends Vitest's expect method with methods from react-testing-library
-expect.extend(matchers);
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
 afterEach(() => {
