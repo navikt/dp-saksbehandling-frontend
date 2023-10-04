4260525b8872446e6b847809c40eb7feb0078e71
Rebecca Gjerstad <rebecca.soraya.gjerstad@nav.no>
2023-10-04 13:08:34 +0200

    NAV-15615 Lagt inn kommentar på hvor datasett skal legges Co-authored-by: Nattaphong Klinjan <nattaphong.klinjan@nav.no>

--- app/routes/saksbehandling.person.$oppgaveId.oversikt._index.tsx
+++ app/routes/saksbehandling.person.$oppgaveId.oversikt._index.tsx
@@ -1,5 +1,7 @@
 import { redirect } from "@remix-run/node";
 
+// Hent hentPersonArbeidssokerStatus her???????
+
 export async function loader() {
   return redirect("rapportering-og-utbetaling");
 }
