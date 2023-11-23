c6175189a91bb27ffec311122494b612bdf71b07
Sindre Sægrov <sindre.segrov@nav.no>
2023-11-23 10:25:47 +0100

    Fjern faro logging for lokal utvikling

--- app/utils/faro.ts
+++ app/utils/faro.ts
@@ -10,9 +10,13 @@ export function initInstrumentation(): void {
 }
 
 export function getFaro(): Faro | null {
-  if (faro != null) return faro;
+  if (faro != null) {
+    return faro;
+  }
+
   faro = initializeFaro({
     url: "https://telemetry.ekstern.dev.nav.no/collect", // required, see below
+    paused: !!process.env.IS_LOCALHOST,
     app: {
       name: "dp-saksbehandling-frontend", // required
       version: "0.1", // optional; useful in Grafana to get diff between versions
