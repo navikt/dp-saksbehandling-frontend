070444a3fa8c1319fa6f031e94a1412e24bd7d7e
Knut Magne Riise <knut.magne.riise@nav.no>
2024-01-31 10:44:10 +0100

    hacke til mock for journalposter og arbeidsforhold også

--- app/routes/saksbehandling.oppgave.$oppgaveId.steg.tsx
+++ app/routes/saksbehandling.oppgave.$oppgaveId.steg.tsx
@@ -1,25 +1,31 @@
 import { Outlet } from "@remix-run/react";
 import { BehandlingStegMenyPunkt } from "~/components/behandling-steg-meny-punkt/BehandlingStegMenyPunkt";
 import styles from "~/route-styles/behandle.module.css";
-import { defer, type LoaderFunctionArgs } from "@remix-run/node";
+import { defer, json, type LoaderFunctionArgs } from "@remix-run/node";
 import invariant from "tiny-invariant";
 import { getSession } from "~/models/auth.server";
 import { hentOppgave } from "~/models/oppgave.server";
 import { hentArbeidsforhold } from "~/models/arbeidsforhold.server";
 import { hentJournalpost } from "~/models/saf.server";
 import { useTypedRouteLoaderData } from "~/hooks/useTypedRouteLoaderData";
+import { masterMenyMock } from "mocks/api-routes/oppgaverResponse";
 
 export async function loader({ params, request }: LoaderFunctionArgs) {
   invariant(params.oppgaveId, "params.oppgaveId er påkrevd");
 
   const session = await getSession(request);
-  const oppgave = await hentOppgave(params.oppgaveId, session);
-
   function hentJournalposter() {
     return Promise.all(
       oppgave.journalposter.map((journalpostId) => hentJournalpost(request, journalpostId)),
     );
   }
+  if (params.oppgaveId === masterMenyMock.uuid)
+    return json({
+      journalposterPromises: await hentJournalposter(),
+      arbeidsforholdPromise: { status: "success", data: [] },
+    }); // ønsker en hardkodet oppgave i test
+
+  const oppgave = await hentOppgave(params.oppgaveId, session);
 
   const journalposterPromises = hentJournalposter();
   const arbeidsforholdResponse = await hentArbeidsforhold(session, oppgave.person);
