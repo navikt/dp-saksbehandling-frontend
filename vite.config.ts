01f9b35b4391ee7ad2ccb8a312255e89640bd6ab
Sindre Sægrov <sindre.segrov@nav.no>
2024-04-16 15:04:41 +0200

    test i dev

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 installGlobals();
 
 export default defineConfig({
-  base: "/",
+  base: "/saksbehandling/",
   plugins: [
     remix({
-      basename: "/saksbehandling",
+      basename: "/saksbehandling/",
     }),
     tsconfigPaths(),
   ],
