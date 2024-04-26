c4f50eaebedcaf643b493ae42cfcaddb28d2082c
Sindre Sægrov <sindre.segrov@nav.no>
2024-04-26 15:21:24 +0200

    Bruk ecs pino format

--- app/utils/logger2.utils.ts
+++ app/utils/logger2.utils.ts
@@ -1,26 +1,10 @@
 import type { Logger, LoggerOptions } from "pino";
 import { pino } from "pino";
+import { ecsFormat } from "@elastic/ecs-pino-format";
 import { getEnv } from "~/utils/env.utils";
 
 const commonConfig: LoggerOptions = {
-  timestamp: false,
-  formatters: {
-    level: (label) => {
-      return { level: label };
-    },
-    log: (object: any) => {
-      if (object.err) {
-        // backendlogger has an Error-instance, frontendlogger has already serialized it
-        const err = object.err instanceof Error ? pino.stdSerializers.err(object.err) : object.err;
-        object.stack_trace = err.stack;
-        object.type = err.type;
-        object.message = err.message;
-        delete object.err;
-      }
-
-      return object;
-    },
-  },
+  ...ecsFormat,
 };
 
 const devConfig: LoggerOptions = {
