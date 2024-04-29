fe8ac5e8c1dcbdc38fed3b743a7a7fbdaa875651
Knut Magne Riise <knut.magne.riise@nav.no>
2024-04-29 14:30:18 +0200

    prøver å fikse sikkerlogger filskriving sin init for pino

--- app/utils/logger.utils.ts
+++ app/utils/logger.utils.ts
@@ -22,11 +22,12 @@ const prodConfig: LoggerOptions = {
 
 export const logger: Logger = pino(getEnv("IS_LOCALHOST") ? devConfig : prodConfig);
 
+const logDestination = getEnv("IS_LOCALHOST") ? "./secure.log" : "/secure-logs/secure.log";
 export const sikkerLogger: Logger = pino(
   {
     formatters: {
       level: (label) => ({ level: label }),
     },
   },
-  destination(getEnv("IS_LOCALHOST") ? "./secure.log" : "/secure-logs/secure.log"),
+  destination(logDestination),
 );
