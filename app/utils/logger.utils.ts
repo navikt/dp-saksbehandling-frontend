9824f64c40f12a585e4d6b0729ade8725283405f
Knut Magne Riise <knut.magne.riise@nav.no>
2024-05-02 09:13:14 +0200

    prøv å initialisere sikkerlogger via pino igjen

--- app/utils/logger.utils.ts
+++ app/utils/logger.utils.ts
@@ -1,7 +1,8 @@
 import type { Logger, LoggerOptions } from "pino";
-import { pino } from "pino";
+import { destination, pino } from "pino";
 import { ecsFormat } from "@elastic/ecs-pino-format";
 import { getEnv } from "~/utils/env.utils";
+import fs from "node:fs";
 
 const devConfig: LoggerOptions = {
   transport: {
@@ -22,12 +23,13 @@ const prodConfig: LoggerOptions = {
 
 export const logger: Logger = pino(getEnv("IS_LOCALHOST") ? devConfig : prodConfig);
 
-/*const logDestination = getEnv("IS_LOCALHOST") ? "./secure.log" : "/secure-logs/secure.log";
+const sikkerLogPath = () =>
+  fs.existsSync("/secure-logs/") ? "/secure-logs/secure.log" : "./secure.log";
 export const sikkerLogger: Logger = pino(
   {
     formatters: {
       level: (label) => ({ level: label }),
     },
   },
-  destination(logDestination),
-);*/
+  destination(sikkerLogPath()),
+);
