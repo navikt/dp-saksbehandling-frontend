58b02108510b4ac74f3caebc91b467c49f309d19
Sindre Sægrov <sindre.segrov@nav.no>
2024-05-06 15:52:26 +0200

    Legg til tester for å sette status filter i oppgaveliste

--- tests/playwright/oppgaveliste.test.ts
+++ tests/playwright/oppgaveliste.test.ts
@@ -1,13 +1,63 @@
 import { expect, test } from "@playwright/test";
 
-test("Klikk på filter under behandling setter tilstand i url", async ({ page, baseURL }) => {
+test("Bruker kan navigere til mine oppgaver", async ({ page, baseURL }) => {
+  await page.goto(`${baseURL}`);
+
+  const mineOppgaverLenke = page.getByRole("link", { name: "Mine oppgaver" });
+  await mineOppgaverLenke.click();
+
+  await expect(page).toHaveURL(`${baseURL}mine-oppgaver?mineOppgaver=true`);
+});
+
+test("Bruker kan navigere til alle oppgaver", async ({ page, baseURL }) => {
+  await page.goto(`${baseURL}`);
+
+  const alleOppgaverLenke = page.getByRole("link", { name: "Alle oppgaver" });
+  await alleOppgaverLenke.click();
+
+  await expect(page).toHaveURL(`${baseURL}alle-oppgaver`);
+});
+
+test("Bruker kan navigere til oppgaver til behandling", async ({ page, baseURL }) => {
+  await page.goto(`${baseURL}`);
+
+  const alleOppgaverLenke = page.getByRole("link", { name: "Oppgaver til behandling" });
+  await alleOppgaverLenke.click();
+
+  await expect(page).toHaveURL(`${baseURL}?tilstand=KLAR_TIL_BEHANDLING`);
+});
+
+test("Klikk på 1 status filter setter tilstand i url og checkbox som checked", async ({
+  page,
+  baseURL,
+}) => {
   await page.goto(`${baseURL}alle-oppgaver`);
 
-  const underBehandlingCheckbox = await page.getByLabel("Under behandling");
+  const underBehandlingCheckbox = page.getByLabel("Under behandling");
   await underBehandlingCheckbox.check();
-  const oppgaveListe = await page.getByRole("table");
 
   await expect(underBehandlingCheckbox).toBeChecked();
   await expect(page).toHaveURL(`${baseURL}alle-oppgaver?tilstand=UNDER_BEHANDLING`);
-  await expect(oppgaveListe).toBeVisible();
+});
+
+test("Klikk på 3 status filter setter tilstand i url og checkbox som checked", async ({
+  page,
+  baseURL,
+}) => {
+  await page.goto(`${baseURL}alle-oppgaver`);
+
+  const klarTilBehandlingCheckbox = page.getByLabel("Klar til behandling");
+  const underBehandlingCheckbox = page.getByLabel("Under behandling");
+  const ferdigBehandlingCheckbox = page.getByLabel("Ferdig behandlet");
+
+  await klarTilBehandlingCheckbox.check();
+  await underBehandlingCheckbox.check();
+  await ferdigBehandlingCheckbox.check();
+
+  await expect(klarTilBehandlingCheckbox).toBeChecked();
+  await expect(underBehandlingCheckbox).toBeChecked();
+  await expect(ferdigBehandlingCheckbox).toBeChecked();
+  await expect(page).toHaveURL(
+    `${baseURL}alle-oppgaver?tilstand=KLAR_TIL_BEHANDLING&tilstand=UNDER_BEHANDLING&tilstand=FERDIG_BEHANDLET`,
+  );
 });
