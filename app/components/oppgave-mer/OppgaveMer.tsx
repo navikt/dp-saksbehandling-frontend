01593a02e592914b619cf2ba4ac2551c3427213f
Sindre Sægrov <sindre.segrov@nav.no>
2024-05-23 08:26:42 +0200

    Fjern utsett oppgave og sett på vent hvis oppgaven er ferdigstilt

--- app/components/oppgave-mer/OppgaveMer.tsx
+++ app/components/oppgave-mer/OppgaveMer.tsx
@@ -4,67 +4,73 @@ import { add, format } from "date-fns";
 import { Button, Checkbox, DatePicker, Modal } from "@navikt/ds-react";
 import { nb } from "date-fns/locale";
 import styles from "./OppgaveMer.module.css";
+import { useTypedRouteLoaderData } from "~/hooks/useTypedRouteLoaderData";
 
 export function OppgaveMer() {
   const fetcher = useFetcher();
+  const { oppgave } = useTypedRouteLoaderData("routes/oppgave.$oppgaveId");
   const ref = useRef<HTMLDialogElement>(null);
   const [utsattTilDato, setUtsattTilDato] = useState<Date | undefined>();
 
   return (
     <div className={styles.container}>
-      <fetcher.Form method="post">
-        <Button
-          size="small"
-          variant="secondary"
-          name="_action"
-          value="legg-tilbake-oppgave"
-          loading={fetcher.state !== "idle"}
-        >
-          Legg oppgave tilbake i køen
-        </Button>
-      </fetcher.Form>
-
-      <Button size="small" variant="secondary" onClick={() => ref.current?.showModal()}>
-        Utsett oppgave
-      </Button>
-
-      <Modal
-        ref={ref}
-        className={styles.modal}
-        header={{ heading: "Utsett oppgave" }}
-        closeOnBackdropClick
-      >
-        <Modal.Body>
+      {oppgave.tilstand !== "FERDIG_BEHANDLET" && (
+        <>
           <fetcher.Form method="post">
-            <DatePicker.Standalone
-              onSelect={(dato) => setUtsattTilDato(dato)}
-              fromDate={new Date()}
-              toDate={add(new Date(), { days: 19 })}
-            />
-
-            <input
-              hidden
-              readOnly
-              name="utsettTilDato"
-              value={utsattTilDato ? format(utsattTilDato, "yyyy-MM-dd", { locale: nb }) : ""}
-            />
-
-            <Checkbox name="beholdOppgave" size="small">
-              Behold oppgave
-            </Checkbox>
-
             <Button
               size="small"
-              variant="primary"
+              variant="secondary"
               name="_action"
-              value="utsett-oppgave"
+              value="legg-tilbake-oppgave"
               loading={fetcher.state !== "idle"}
             >
-              Utsett oppgave
+              Legg oppgave tilbake i køen
             </Button>
           </fetcher.Form>
-        </Modal.Body>
-      </Modal>
+
+          <Button size="small" variant="secondary" onClick={() => ref.current?.showModal()}>
+            Sett på vent
+          </Button>
+
+          <Modal
+            ref={ref}
+            className={styles.modal}
+            header={{ heading: "Sett på vent" }}
+            closeOnBackdropClick
+          >
+            <Modal.Body>
+              <fetcher.Form method="post">
+                <DatePicker.Standalone
+                  onSelect={(dato) => setUtsattTilDato(dato)}
+                  fromDate={new Date()}
+                  toDate={add(new Date(), { days: 19 })}
+                />
+
+                <input
+                  hidden
+                  readOnly
+                  name="utsettTilDato"
+                  value={utsattTilDato ? format(utsattTilDato, "yyyy-MM-dd", { locale: nb }) : ""}
+                />
+
+                <Checkbox name="beholdOppgave" size="small">
+                  Behold oppgave
+                </Checkbox>
+
+                <Button
+                  size="small"
+                  variant="primary"
+                  name="_action"
+                  value="utsett-oppgave"
+                  loading={fetcher.state !== "idle"}
+                >
+                  Sett på vent
+                </Button>
+              </fetcher.Form>
+            </Modal.Body>
+          </Modal>
+        </>
+      )}
     </div>
   );
 }
