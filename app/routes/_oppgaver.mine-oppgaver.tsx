ff4ecdb612efd180e60bf62f695493a1dabea775
Sindre Sægrov <sindre.segrov@nav.no>
2024-06-18 14:07:18 +0200

    Legg til oppgaveteller på mine og alle oppgaver view

--- app/routes/_oppgaver.mine-oppgaver.tsx
+++ app/routes/_oppgaver.mine-oppgaver.tsx
@@ -68,7 +68,7 @@ export default function Saksbehandling() {
       </aside>
 
       <main>
-        <OppgaveListe oppgaver={oppgaver} />
+        <OppgaveListe oppgaver={oppgaver} visAntallOppgaver={true} />
       </main>
     </div>
   );
