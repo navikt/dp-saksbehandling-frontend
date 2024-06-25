43b3c4704cd20040e895b53d6c0b4b3300557fe6
Sindre Sægrov <sindre.segrov@nav.no>
2024-06-25 11:28:05 +0200

    Legg til cache på saksbehandler

--- app/models/saksbehandler.server.ts
+++ app/models/saksbehandler.server.ts
@@ -1,6 +1,7 @@
 import { getHeaders } from "~/utils/fetch.utils";
 import { getMicrosoftOboToken } from "~/utils/auth.utils.server";
 import { logger } from "~/utils/logger.utils";
+import { cache } from "~/utils/cache.utils";
 
 export interface ISaksbehandler {
   onPremisesSamAccountName: string; // Dette er saksbehandlerIdent
@@ -10,6 +11,12 @@ export interface ISaksbehandler {
 }
 
 export async function getSaksbehandler(request: Request): Promise<ISaksbehandler> {
+  const saksbehandler = cache.get<ISaksbehandler>("saksbehandler");
+
+  if (saksbehandler !== undefined) {
+    return saksbehandler;
+  }
+
   try {
     const oboToken = await getMicrosoftOboToken(request);
 
@@ -20,7 +27,10 @@ export async function getSaksbehandler(request: Request): Promise<ISaksbehandler
       headers: getHeaders(oboToken),
     });
 
-    return await data.json();
+    const saksbehandler = await data.json();
+    // cache saksbehandler for 12 timer
+    cache.set<ISaksbehandler>("saksbehandler", saksbehandler, 60 * 60 * 12);
+    return saksbehandler;
   } catch (e) {
     logger.warn(`Klarte ikke hente saksbehandler status: 401`);
     throw new Response("Unauthorized", { status: 401 });
