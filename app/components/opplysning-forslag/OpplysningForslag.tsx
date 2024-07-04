e6b640768a13aa258c49bc89c8d76ae0a5cc1c0f
Rudi Wei Yu <rudi.wei.yu@nav.no>
2024-07-04 07:59:13 +0200

    La til ordentlig data i Behandlingsoversikt

--- app/components/opplysning-forslag/OpplysningForslag.tsx
+++ app/components/opplysning-forslag/OpplysningForslag.tsx
@@ -1,8 +1,36 @@
 import { Table } from "@navikt/ds-react";
 import styles from "./OpplysningForslag.module.css";
-import { CheckmarkCircleIcon } from "@navikt/aksel-icons";
+import { CheckmarkCircleIcon, XMarkOctagonIcon } from "@navikt/aksel-icons";
+import type { IOpplysning } from "~/models/behandling.server";
 
-export default function OpplysningForslag() {
+interface IProps {
+  opplysninger: IOpplysning[];
+}
+
+const finnOpplysningMedNavn = (navn: string, opplysninger: IOpplysning[]) => {
+  return opplysninger.filter((opplysning) => {
+    return opplysning.navn === navn;
+  })[0];
+};
+
+export default function OpplysningForslag(props: IProps) {
+  const harKravDagpenger = finnOpplysningMedNavn("Krav på dagpenger", props.opplysninger);
+  const kravTilAlder = finnOpplysningMedNavn("Oppfyller kravet til alder", props.opplysninger);
+  const kravTilInntekt = finnOpplysningMedNavn("Krav til minsteinntekt", props.opplysninger);
+  const beregnetVirkningsTidspunkt = finnOpplysningMedNavn(
+    "EttBeregnetVirkningstidspunkt",
+    props.opplysninger,
+  );
+  const soknadsDato = finnOpplysningMedNavn("Søknadsdato", props.opplysninger);
+  const kravTilArbeidssøker = finnOpplysningMedNavn("Krav til arbeidssøker", props.opplysninger);
+  const registrertSomArbeidssoker = finnOpplysningMedNavn(
+    "Registrert som arbeidssøker på søknadstidspunktet",
+    props.opplysninger,
+  );
+
+  console.log(kravTilInntekt.verdi);
+
+  const resultStyle = harKravDagpenger.verdi === "true" ? styles.approved : styles.denied;
   return (
     <div className={styles.container}>
       <h1>Behandlingsoversikt</h1>
@@ -11,34 +39,69 @@ export default function OpplysningForslag() {
         behandlingen. Se nøkkelpunkter for å få en oversikt.
       </p>
       <Table>
-        <Table.Row className={styles.success}>
+        <Table.Row className={resultStyle}>
           <Table.DataCell>
-            <span>
-              <CheckmarkCircleIcon fontSize="1em" />
-              Bruker har rett til dagpenger
-            </span>
+            {harKravDagpenger.verdi === "true" ? (
+              <>
+                <CheckmarkCircleIcon />
+                Bruker har rett til dagpenger
+              </>
+            ) : (
+              <>
+                <XMarkOctagonIcon />
+                Bruker har fått avslag på dagpenger
+              </>
+            )}
           </Table.DataCell>
           <Table.DataCell></Table.DataCell>
         </Table.Row>
         <Table.Row>
-          <Table.DataCell>Virkningstidspunkt</Table.DataCell>
-          <Table.DataCell>12.05.2023</Table.DataCell>
+          <Table.DataCell>Et Beregnet Virkningstidspunkt</Table.DataCell>
+          <Table.DataCell>{beregnetVirkningsTidspunkt?.verdi}</Table.DataCell>
         </Table.Row>
         <Table.Row>
           <Table.DataCell>Søknadsdato</Table.DataCell>
-          <Table.DataCell>05.05.2023</Table.DataCell>
+          <Table.DataCell>{soknadsDato?.verdi}</Table.DataCell>
         </Table.Row>
         <Table.Row>
-          <Table.DataCell>Dagpengeperiode</Table.DataCell>
-          <Table.DataCell>104 uker</Table.DataCell>
+          <Table.DataCell>Krav til alder</Table.DataCell>
+          <Table.DataCell>{kravTilAlder?.verdi === "true" ? "Ja" : "Nei"}</Table.DataCell>
         </Table.Row>
         <Table.Row>
-          <Table.DataCell>Egenandel</Table.DataCell>
-          <Table.DataCell>4 818 kr</Table.DataCell>
+          <Table.DataCell>Krav til Minsteinntekt</Table.DataCell>
+          <Table.DataCell>{kravTilInntekt?.verdi === "true" ? "Ja" : "Nei"}</Table.DataCell>
+        </Table.Row>
+        {kravTilInntekt?.verdi === "false" && (
+          <>
+            <Table.Row>
+              <Table.DataCell>Arbeidsinntekt siste 12 mnd</Table.DataCell>
+              <Table.DataCell>
+                {parseFloat(
+                  finnOpplysningMedNavn("Arbeidsinntekt siste 12 mnd", props.opplysninger).verdi,
+                )}{" "}
+                Kr
+              </Table.DataCell>
+            </Table.Row>
+            <Table.Row>
+              <Table.DataCell>Arbeidsinntekt siste 36 mnd</Table.DataCell>
+              <Table.DataCell>
+                {parseFloat(
+                  finnOpplysningMedNavn("Arbeidsinntekt siste 36 mnd", props.opplysninger).verdi,
+                )}{" "}
+                Kr
+              </Table.DataCell>
+            </Table.Row>
+          </>
+        )}
+        <Table.Row>
+          <Table.DataCell>Krav til Arbeidssøker</Table.DataCell>
+          <Table.DataCell>{kravTilArbeidssøker?.verdi === "true" ? "Ja" : "Nei"}</Table.DataCell>
         </Table.Row>
         <Table.Row>
-          <Table.DataCell>Dagsats</Table.DataCell>
-          <Table.DataCell>1 606 kr</Table.DataCell>
+          <Table.DataCell>Registrert som arbeidssøker på søknadstidspunktet</Table.DataCell>
+          <Table.DataCell>
+            {registrertSomArbeidssoker.verdi === "true" ? "Ja" : "Nei"}
+          </Table.DataCell>
         </Table.Row>
       </Table>
     </div>
--- app/routes/oppgave.$oppgaveId.oversikt.tsx
+++ app/routes/oppgave.$oppgaveId.oversikt.tsx
@@ -1,10 +1,13 @@
 import { Tabs } from "@navikt/ds-react";
 import OpplysningForslag from "~/components/opplysning-forslag/OpplysningForslag";
+import { useTypedRouteLoaderData } from "~/hooks/useTypedRouteLoaderData";
 
 export default function Oversikt() {
+  const { behandling } = useTypedRouteLoaderData("routes/oppgave.$oppgaveId");
+
   return (
     <Tabs.Panel value="oversikt">
-      <OpplysningForslag />
+      <OpplysningForslag opplysninger={behandling.opplysning} />
     </Tabs.Panel>
   );
 }
