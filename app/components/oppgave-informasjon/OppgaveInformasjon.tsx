790e89001542188a9352e9ece445f3b6f5aded9c
Sindre Sægrov <sindre.segrov@nav.no>
2024-07-30 10:02:29 +0200

    Sett dokumenter tab som default

--- app/components/oppgave-informasjon/OppgaveInformasjon.tsx
+++ app/components/oppgave-informasjon/OppgaveInformasjon.tsx
@@ -5,22 +5,22 @@ import { OppgaveLenker } from "~/components/oppgave-lenker/OppgaveLenker";
 
 export function OppgaveInformasjon() {
   return (
-    <Tabs defaultValue="informasjon" size="small">
+    <Tabs defaultValue="dokumenter" size="small">
       <Tabs.List>
+        <Tabs.Tab value="dokumenter" label="Dokumenter" icon={<FilesIcon title="Dokumenter" />} />
         <Tabs.Tab
           value="informasjon"
           label="Informasjon"
           icon={<DatabaseIcon title="Informasjon" />}
         />
-        <Tabs.Tab value="dokumenter" label="Dokumenter" icon={<FilesIcon title="Dokumenter" />} />
       </Tabs.List>
-
-      <Tabs.Panel value="informasjon">
-        <OppgaveLenker />
-      </Tabs.Panel>
 
       <Tabs.Panel value="dokumenter">
         <DokumentOversikt />
+      </Tabs.Panel>
+
+      <Tabs.Panel value="informasjon">
+        <OppgaveLenker />
       </Tabs.Panel>
     </Tabs>
   );
