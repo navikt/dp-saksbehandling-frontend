e853536cb09b7dd164c56f2b968a61b2374f6e07
Rudi Wei Yu <rudi.wei.yu@nav.no>
2024-08-13 13:27:38 +0200

    Nye oversikt over behandling fra robot

--- app/components/opplysning-forslag/OpplysningForslag.tsx
+++ app/components/opplysning-forslag/OpplysningForslag.tsx
@@ -1,5 +1,6 @@
 import { BodyLong, Heading, Table } from "@navikt/ds-react";
 import { CheckmarkCircleIcon, XMarkOctagonIcon } from "@navikt/aksel-icons";
+import { parse, format } from "date-fns";
 import type { IOpplysning } from "~/models/behandling.server";
 import styles from "./OpplysningForslag.module.css";
 
@@ -15,20 +16,15 @@ function finnOpplysningMedNavn(navn: string, opplysninger: IOpplysning[]) {
 
 export function OpplysningForslag(props: IProps) {
   const harKravDagpenger = finnOpplysningMedNavn("Krav på dagpenger", props.opplysninger);
-  const kravTilAlder = finnOpplysningMedNavn("Oppfyller kravet til alder", props.opplysninger);
+  const resultStyle = harKravDagpenger.verdi === "true" ? styles.approved : styles.denied;
+
+  const kravTilArbeidssoker = finnOpplysningMedNavn("Krav til arbeidssøker", props.opplysninger);
   const kravTilInntekt = finnOpplysningMedNavn("Krav til minsteinntekt", props.opplysninger);
-  const beregnetVirkningsTidspunkt = finnOpplysningMedNavn(
+  const beregnetVirkningstidspunkt = finnOpplysningMedNavn(
     "EttBeregnetVirkningstidspunkt",
     props.opplysninger,
   );
-  const soknadsDato = finnOpplysningMedNavn("Søknadsdato", props.opplysninger);
-  const kravTilArbeidssoker = finnOpplysningMedNavn("Krav til arbeidssøker", props.opplysninger);
-  const registrertSomArbeidssoker = finnOpplysningMedNavn(
-    "Registrert som arbeidssøker på søknadstidspunktet",
-    props.opplysninger,
-  );
 
-  const resultStyle = harKravDagpenger.verdi === "true" ? styles.approved : styles.denied;
   return (
     <div className={styles.container}>
       <Heading size={"large"}>Behandlingsoversikt</Heading>
@@ -38,10 +34,10 @@ export function OpplysningForslag(props: IProps) {
         behandlingen. Se nøkkelpunkter for å få en oversikt.
       </BodyLong>
 
-      <Table className={"table--subtle-zebra"}>
-        <Table.Body>
+      <Table zebraStripes>
+        <Table.Header>
           <Table.Row className={resultStyle}>
-            <Table.DataCell colSpan={2}>
+            <Table.HeaderCell colSpan={3}>
               {harKravDagpenger.verdi === "true" ? (
                 <span className={styles.result}>
                   <CheckmarkCircleIcon />
@@ -53,61 +49,57 @@ export function OpplysningForslag(props: IProps) {
                   Bruker har fått avslag på dagpenger
                 </span>
               )}
-            </Table.DataCell>
+            </Table.HeaderCell>
           </Table.Row>
-
+        </Table.Header>
+        <Table.Body>
           <Table.Row>
-            <Table.DataCell>Et Beregnet Virkningstidspunkt</Table.DataCell>
-            <Table.DataCell>{beregnetVirkningsTidspunkt?.verdi}</Table.DataCell>
+            <Table.HeaderCell scope="row">Dagpenger</Table.HeaderCell>
+            <Table.DataCell>Ordinær</Table.DataCell>
+            <Table.DataCell>Søknad</Table.DataCell>
           </Table.Row>
-
           <Table.Row>
-            <Table.DataCell>Søknadsdato</Table.DataCell>
-            <Table.DataCell>{soknadsDato?.verdi}</Table.DataCell>
+            <Table.HeaderCell scope="row">§4-16 Gjenoppptak</Table.HeaderCell>
+            <Table.DataCell>Nei</Table.DataCell>
+            <Table.DataCell>Søknad</Table.DataCell>
           </Table.Row>
-
           <Table.Row>
-            <Table.DataCell>Krav til alder</Table.DataCell>
-            <Table.DataCell>{kravTilAlder?.verdi === "true" ? "Ja" : "Nei"}</Table.DataCell>
-          </Table.Row>
-
-          <Table.Row>
-            <Table.DataCell>Krav til Minsteinntekt</Table.DataCell>
+            <Table.HeaderCell scope="row">§4-4 Krav til minsteinntekt</Table.HeaderCell>
             <Table.DataCell>{kravTilInntekt?.verdi === "true" ? "Ja" : "Nei"}</Table.DataCell>
+            <Table.DataCell>Søknad</Table.DataCell>
           </Table.Row>
-
-          {kravTilInntekt?.verdi === "false" && (
-            <>
-              <Table.Row>
-                <Table.DataCell>Arbeidsinntekt siste 12 mnd</Table.DataCell>
-                <Table.DataCell>
-                  {parseFloat(
-                    finnOpplysningMedNavn("Arbeidsinntekt siste 12 mnd", props.opplysninger).verdi,
-                  )}{" "}
-                  Kr
-                </Table.DataCell>
-              </Table.Row>
-
-              <Table.Row>
-                <Table.DataCell>Arbeidsinntekt siste 36 mnd</Table.DataCell>
-                <Table.DataCell>
-                  {`${parseFloat(
-                    finnOpplysningMedNavn("Arbeidsinntekt siste 36 mnd", props.opplysninger).verdi,
-                  )} kr`}
-                </Table.DataCell>
-              </Table.Row>
-            </>
-          )}
+          {/* <Table.Row>
+            <Table.HeaderCell scope="row">§4-2 Opphold i Norge</Table.HeaderCell>
+            <Table.DataCell>Nei</Table.DataCell>
+            <Table.DataCell>Søknad</Table.DataCell>
+          </Table.Row>
           <Table.Row>
-            <Table.DataCell>Krav til Arbeidssøker</Table.DataCell>
+            <Table.HeaderCell scope="row">§4-8 Meldeplikt og møteplikt</Table.HeaderCell>
+            <Table.DataCell>Nei</Table.DataCell>
+            <Table.DataCell>Søknad</Table.DataCell>
+          </Table.Row> */}
+          <Table.Row>
+            <Table.HeaderCell scope="row">§4-Reel arbeidssøker</Table.HeaderCell>
             <Table.DataCell>{kravTilArbeidssoker?.verdi === "true" ? "Ja" : "Nei"}</Table.DataCell>
+            <Table.DataCell>Søknad</Table.DataCell>
           </Table.Row>
-
+          {/* <Table.Row>
+            <Table.HeaderCell scope="row">
+              §4-3 Tap av arbeidstid og arbeidsinntekt
+            </Table.HeaderCell>
+            <Table.DataCell>Nei</Table.DataCell>
+            <Table.DataCell>Søknad</Table.DataCell>
+          </Table.Row> */}
           <Table.Row>
-            <Table.DataCell>Registrert som arbeidssøker på søknadstidspunktet</Table.DataCell>
+            <Table.HeaderCell scope="row">§3A-1 Søknadstidspunkt</Table.HeaderCell>
             <Table.DataCell>
-              {registrertSomArbeidssoker.verdi === "true" ? "Ja" : "Nei"}
+              Virkningstidspunkt{" "}
+              {format(
+                parse(beregnetVirkningstidspunkt?.verdi, "yyyy-MM-dd", new Date()),
+                "dd.MM.yyyy",
+              )}
             </Table.DataCell>
+            <Table.DataCell>Søknad</Table.DataCell>
           </Table.Row>
         </Table.Body>
       </Table>
