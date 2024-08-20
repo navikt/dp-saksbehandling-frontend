56c70396b9a81002fd589a10d3989d4c7d172d20
Sindre Sægrov <sindre.segrov@nav.no>
2024-08-20 15:36:53 +0200

    Test i dev

--- app/routes/oppgave.$oppgaveId.melding-om-vedtak.tsx
+++ app/routes/oppgave.$oppgaveId.melding-om-vedtak.tsx
@@ -10,11 +10,28 @@ import styles from "../route-styles/melding-om-vedtak.module.css";
 import { hentOppgave } from "~/models/oppgave.server";
 import invariant from "tiny-invariant";
 import { hentMeldingOmVedtak } from "~/models/melding-om-vedtak.server";
+import { getMeldingOmVedtakOboToken } from "~/utils/auth.utils.server";
+import { getEnv } from "~/utils/env.utils";
+import { getHeaders } from "~/utils/fetch.utils";
+import { handleErrorResponse } from "~/utils/error-response.server";
 
 export async function loader({ params, request }: LoaderFunctionArgs) {
   invariant(params.oppgaveId, "params.oppgaveId er påkrevd");
   const oppgave = await hentOppgave(request, params.oppgaveId);
   const meldingOmVedtak = await hentMeldingOmVedtak(request, oppgave.behandlingId);
+
+  const onBehalfOfToken = await getMeldingOmVedtakOboToken(request);
+
+  const url = `${getEnv("DP_MELDING_OM_VEDTAK_URL")}/isalive`;
+  const response = await fetch(url, {
+    method: "GET",
+    headers: getHeaders(onBehalfOfToken),
+  });
+
+  if (!response.ok) {
+    handleErrorResponse(response);
+  }
+  console.log(await response.json());
 
   const brevBlokkIder = meldingOmVedtak.map((blokk) => blokk.tekstId);
   const sanityBrevBlokker = await sanityClient.fetch<ISanityBrevBlokk[]>(
