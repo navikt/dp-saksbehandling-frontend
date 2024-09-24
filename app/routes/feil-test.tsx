ede7013acb40a585b0dd5023ea3c5c9764d63720
Sindre Sægrov <sindre.segrov@nav.no>
2024-09-24 15:50:52 +0200

    Slett gamle feil test ruter og lag 1 for å teste alle

---
+++ app/routes/feil-test.tsx
@@ -0,0 +1,92 @@
+import { logger } from "~/utils/logger.utils";
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+
+export async function action({ request, params }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const errorTest = formData.get("errorTest") as string;
+
+  if (errorTest === "throwError") {
+    throw new Error("Throw error server side");
+  }
+
+  if (errorTest === "throwResponse") {
+    throw new Response("Throw error server side", { status: 500 });
+  }
+  if (errorTest === "logger.error") {
+    logger.error("logger.error server side");
+    return null;
+  }
+  if (errorTest === "console.error") {
+    console.error("console.error server side");
+  }
+}
+
+export default function Time() {
+  function throwError() {
+    throw new Error("Throw error client side");
+  }
+
+  function consoleError() {
+    console.error("console.error client side");
+  }
+
+  function loggerError() {
+    logger.error("logger.error client side");
+  }
+
+  return (
+    <div>
+      <h1>Test error</h1>
+      <h2>Client side</h2>
+      <div>
+        <button className={"my-4"} onClick={throwError}>
+          Throw error client side
+        </button>
+      </div>
+      <div>
+        <button className={"my-4"} onClick={consoleError}>
+          console.error client side
+        </button>
+      </div>
+      <div>
+        <button className={"my-4"} onClick={loggerError}>
+          logger.error client side
+        </button>
+      </div>
+
+      <h2>Server side</h2>
+      <Form method="post">
+        <input type="text" name="errorTest" value={"throwError"} readOnly={true} hidden={true} />
+
+        <button className={"my-4"} type="submit">
+          Throw error server side
+        </button>
+      </Form>
+
+      <Form method="post">
+        <input type="text" name="errorTest" value={"throwResponse"} readOnly={true} hidden={true} />
+
+        <button className={"my-4"} type="submit">
+          Throw error response server side
+        </button>
+      </Form>
+
+      <Form method="post">
+        <input type="text" name="errorTest" value={"console.error"} readOnly={true} hidden={true} />
+
+        <button className={"my-4"} type="submit">
+          console.error server side
+        </button>
+      </Form>
+
+      <Form method="post">
+        <input type="text" name="errorTest" value={"logger.error"} readOnly={true} hidden={true} />
+
+        <button className={"my-4"} type="submit">
+          logger.error server side
+        </button>
+      </Form>
+    </div>
+  );
+}
