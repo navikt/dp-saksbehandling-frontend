32a0e2172923417a3f0d4cd48174a27cb6c6c28b
Giao The Cung <giaothe@gmail.com>
2024-10-10 09:55:45 +0200

    Bruk production 100%

--- app/sanity/sanity-client.ts
+++ app/sanity/sanity-client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client";
 
 export const sanityConfig = {
-  dataset: process.env.SANITY_DATASET || "production",
+  dataset: "production",
   projectId: process.env.SANITY_PROJECT_ID || "rt6o382n",
   useCdn: process.env.NODE_ENV === "production",
   token: process.env.SANITY_ACCESS_TOKEN || "",
