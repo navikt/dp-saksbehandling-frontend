80729792cf8756d0a5f20773713148646293296b
Sindre Sægrov <sindre.segrov@nav.no>
2025-01-15 15:36:45 +0100

    Legg til melding om vedtak provider på se route

--- app/routes/oppgave.$oppgaveId.se.tsx
+++ app/routes/oppgave.$oppgaveId.se.tsx
@@ -1,33 +1,52 @@
 import { DocPencilIcon } from "@navikt/aksel-icons";
-import { Tabs } from "@navikt/ds-react";
+import { Alert, Tabs } from "@navikt/ds-react";
 import { Outlet } from "@remix-run/react";
 
 import { Behandling } from "~/components/behandling/Behandling";
 import { OppgaveInformasjon } from "~/components/oppgave-informasjon/OppgaveInformasjon";
+import { MeldingOmVedtakProvider } from "~/context/melding-om-vedtak-context";
+import { useTypedRouteLoaderData } from "~/hooks/useTypedRouteLoaderData";
 import styles from "~/route-styles/oppgave.module.css";
 
 export default function Oppgave() {
+  const { oppgave, meldingOmVedtak } = useTypedRouteLoaderData("routes/oppgave.$oppgaveId");
+
+  if (!meldingOmVedtak) {
+    return (
+      <Alert variant="error">
+        Kan ikke hente melding om vedtak for oppgave i tilstand {oppgave.tilstand}. Ta kontakt med
+        utviklere.
+      </Alert>
+    );
+  }
+
   return (
-    <div className={styles.behandling}>
-      <div className={"card"}>
-        <Tabs size="medium" defaultValue="behandling">
-          <div>
-            <Tabs.List>
-              <Tabs.Tab value="behandling" label="Redigere opplysninger" icon={<DocPencilIcon />} />
-            </Tabs.List>
-          </div>
+    <MeldingOmVedtakProvider utvidedeBeskrivelser={meldingOmVedtak.utvidedeBeskrivelser}>
+      <div className={styles.behandling}>
+        <div className={"card"}>
+          <Tabs size="medium" defaultValue="behandling">
+            <div>
+              <Tabs.List>
+                <Tabs.Tab
+                  value="behandling"
+                  label="Redigere opplysninger"
+                  icon={<DocPencilIcon />}
+                />
+              </Tabs.List>
+            </div>
 
-          <Tabs.Panel value="behandling">
-            <Behandling readonly={true} />
-          </Tabs.Panel>
-        </Tabs>
+            <Tabs.Panel value="behandling">
+              <Behandling readonly={true} />
+            </Tabs.Panel>
+          </Tabs>
+        </div>
+
+        <div className={"card"}>
+          <OppgaveInformasjon />
+        </div>
+
+        <Outlet />
       </div>
-
-      <div className={"card"}>
-        <OppgaveInformasjon />
-      </div>
-
-      <Outlet />
-    </div>
+    </MeldingOmVedtakProvider>
   );
 }
