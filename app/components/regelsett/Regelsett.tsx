9bb028629100527d9f84f1de79f05eddbd2a4889
Sindre Sægrov <sindre.segrov@nav.no>
2025-01-23 11:07:45 +0100

    Splitt opplysningliste basert på formål

--- app/components/regelsett/Regelsett.tsx
+++ app/components/regelsett/Regelsett.tsx
@@ -19,21 +19,117 @@ export function Regelsett({ aktivtRegelsett, readonly }: IProps) {
     .filter((opplysning) => opplysning !== undefined)
     .filter((opplysning) => opplysning.synlig);
 
+  const brukerOpplysninger = aktivtRegelsettOpplysninger.filter(
+    (opplysning) => opplysning?.formål === "Bruker",
+  );
+
+  const registerOpplysninger = aktivtRegelsettOpplysninger.filter(
+    (opplysning) => opplysning?.formål === "Register",
+  );
+
+  const legacyOpplysninger = aktivtRegelsettOpplysninger.filter(
+    (opplysning) => opplysning?.formål === "Legacy",
+  );
+
+  const regelOpplysninger = aktivtRegelsettOpplysninger.filter(
+    (opplysning) => opplysning?.formål === "Regel",
+  );
+
+  const mellomstegOpplysninger = aktivtRegelsettOpplysninger.filter(
+    (opplysning) => opplysning?.formål === "Mellomsteg",
+  );
+
   return (
     <div>
       <Heading className={styles.regelsettHeading} size="medium">
         {aktivtRegelsett.navn}
       </Heading>
       <Avklaringer avklaringer={aktivtRegelsett.avklaringer} />
-      <ul className={styles.opplysningListe}>
-        {aktivtRegelsettOpplysninger.map((opplysning) => (
-          <OpplysningLinje
-            key={`${opplysning.id}-${opplysning.verdi}`}
-            opplysning={opplysning}
-            readonly={readonly}
-          />
-        ))}
-      </ul>
+
+      {brukerOpplysninger.length > 0 && (
+        <>
+          <Heading size={"xsmall"} className={styles.opplysningListeHeading}>
+            Brukerdata
+          </Heading>
+          <ul className={styles.opplysningListe}>
+            {brukerOpplysninger.map((opplysning) => (
+              <OpplysningLinje
+                key={`${opplysning.id}-${opplysning.verdi}`}
+                opplysning={opplysning}
+                readonly={readonly}
+              />
+            ))}
+          </ul>
+        </>
+      )}
+
+      {registerOpplysninger.length > 0 && (
+        <>
+          <Heading size={"xsmall"} className={styles.opplysningListeHeading}>
+            Registerdata
+          </Heading>
+          <ul className={styles.opplysningListe}>
+            {registerOpplysninger.map((opplysning) => (
+              <OpplysningLinje
+                key={`${opplysning.id}-${opplysning.verdi}`}
+                opplysning={opplysning}
+                readonly={readonly}
+              />
+            ))}
+          </ul>
+        </>
+      )}
+
+      {legacyOpplysninger.length > 0 && (
+        <>
+          <Heading size={"xsmall"} className={styles.opplysningListeHeading}>
+            Legacy
+          </Heading>
+          <ul className={styles.opplysningListe}>
+            {legacyOpplysninger.map((opplysning) => (
+              <OpplysningLinje
+                key={`${opplysning.id}-${opplysning.verdi}`}
+                opplysning={opplysning}
+                readonly={readonly}
+              />
+            ))}
+          </ul>
+        </>
+      )}
+
+      {mellomstegOpplysninger.length > 0 && (
+        <>
+          <Heading size={"xsmall"} className={styles.opplysningListeHeading}>
+            Mellomsteg
+          </Heading>
+          <ul className={styles.opplysningListe}>
+            {mellomstegOpplysninger.map((opplysning) => (
+              <OpplysningLinje
+                key={`${opplysning.id}-${opplysning.verdi}`}
+                opplysning={opplysning}
+                readonly={readonly}
+              />
+            ))}
+          </ul>
+        </>
+      )}
+
+      {regelOpplysninger.length > 0 && (
+        <>
+          <Heading size={"xsmall"} className={styles.opplysningListeHeading}>
+            Behandling
+          </Heading>
+          <ul className={styles.opplysningListe}>
+            {regelOpplysninger.map((opplysning) => (
+              <OpplysningLinje
+                key={`${opplysning.id}-${opplysning.verdi}`}
+                opplysning={opplysning}
+                readonly={readonly}
+              />
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
