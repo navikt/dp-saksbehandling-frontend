c37f4956b6787163d1e89420508c4006667ac2f6
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-03-06 12:49:03 +0100

    Rydd opp i feil rebase

--- app/models/orkestrator-opplysning.server.ts
+++ app/models/orkestrator-opplysning.server.ts
@@ -1,16 +1,17 @@
+import { IOrkestratorBarnOpplysning } from "mocks/data/mock-orkestrator-barn-opplysninger";
+import { IOrkestratorLand } from "mocks/data/mock-orkestrator-land-lister";
+
 import { getSoknadOrkestratorOboToken } from "~/utils/auth.utils.server";
 import { getEnv } from "~/utils/env.utils";
 import { handleErrorResponse } from "~/utils/error-response.server";
 import { getHeaders } from "~/utils/fetch.utils";
 
-import { IOrkestratorBarnOpplysning } from "../../mocks/data/mock-orkestrator-barn-opplysninger";
-
 export async function hentOrkestratorBarnOpplysninger(
   request: Request,
-  oppgaveId: string,
+  soknadId: string,
 ): Promise<IOrkestratorBarnOpplysning[]> {
   const onBehalfOfToken = await getSoknadOrkestratorOboToken(request);
-  const url = `${getEnv("DP_SOKNAD_ORKESTRATOR_URL")}/opplysninger/${oppgaveId}/barn`;
+  const url = `${getEnv("DP_SOKNAD_ORKESTRATOR_URL")}/opplysninger/${soknadId}/barn`;
 
   const response = await fetch(url, {
     method: "GET",
@@ -21,5 +22,40 @@ export async function hentOrkestratorBarnOpplysninger(
     handleErrorResponse(response);
   }
 
+  return await response.json();
+}
+
+export async function oppdaterOrkestratorBarnOpplysning(
+  request: Request,
+  soknadId: string,
+  barnOpplysning: IOrkestratorBarnOpplysning,
+): Promise<IOrkestratorLand[]> {
+  const onBehalfOfToken = await getSoknadOrkestratorOboToken(request);
+  const url = `${getEnv("DP_SOKNAD_ORKESTRATOR_URL")}/opplysninger/${soknadId}/barn/oppdater`;
+
+  const response = await fetch(url, {
+    method: "PUT",
+    headers: getHeaders(onBehalfOfToken),
+    body: JSON.stringify({ barnOpplysning }),
+  });
+
+  if (!response.ok) {
+    handleErrorResponse(response);
+  }
+
+  return await response.json();
+}
+
+export async function hentOrkestratorLandListe(request: Request): Promise<IOrkestratorLand[]> {
+  const onBehalfOfToken = await getSoknadOrkestratorOboToken(request);
+  const url = `${getEnv("DP_SOKNAD_ORKESTRATOR_URL")}/land`;
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: getHeaders(onBehalfOfToken),
+  });
+  if (!response.ok) {
+    handleErrorResponse(response);
+  }
   return await response.json();
 }
