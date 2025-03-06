80cbd6825c5eed140c851ba266e36aa5f304d565
Nattaphong Klinjan <nattaphong.klinjan@nav.no>
2025-03-06 12:02:40 +0100

    Rydde opp endringer som ikke relevant med branchen

--- app/components/behandling/Behandling.tsx
+++ app/components/behandling/Behandling.tsx
@@ -16,7 +16,6 @@ export function Behandling(props: IProps) {
   const { behandling } = useTypedRouteLoaderData("routes/oppgave.$oppgaveId");
   const [aktivtRegelsett, setAktivtRegelsett] = useState<IRegelsett>(behandling.vilkår[0]);
 
-
   useEffect(() => {
     if (behandling) {
       const alleRegelsett = [...behandling.vilkår, ...behandling.fastsettelser];
