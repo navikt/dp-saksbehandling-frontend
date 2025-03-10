a015c83821032417ca9c19541eebfffe6fd27aa4
Sindre Sægrov <sindre.segrov@nav.no>
2025-03-10 12:31:30 +0100

    Prøv uten cdn

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === "production"
-      ? "https://cdn.nav.no/teamdagpenger/dp-saksbehandling-frontend/client/"
-      : "/saksbehandling",
+  // base:
+  //   process.env.NODE_ENV === "production"
+  //     ? "https://cdn.nav.no/teamdagpenger/dp-saksbehandling-frontend/client/"
+  //     : "/saksbehandling",
+  base: "/saksbehandling",
   server: {
     port: 3000,
   },
