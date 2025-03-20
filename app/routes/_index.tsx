2bd19cc5f782fb047d0f70da4fe1e137a59e3277
Sindre Sægrov <sindre.segrov@nav.no>
2025-03-20 12:06:19 +0100

    Oppdater aktivtsøk context når search params endres

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -79,7 +79,7 @@ export default function Saksbehandling() {
 
   useEffect(() => {
     setAktivtOppgaveSok(searchParams.toString());
-  }, []);
+  }, [searchParams]);
 
   return (
     <div className={styles.container}>
