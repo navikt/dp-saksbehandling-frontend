1572980ab06e5c0595088e3e8335c41b8080af8f
Sindre Sægrov <sindre.segrov@nav.no>
2025-03-28 13:48:48 +0100

    Legg til melding om vedtak og SB begrunnelse på se view

--- app/routes/oppgave.$oppgaveId.se.tsx
+++ app/routes/oppgave.$oppgaveId.se.tsx
@@ -1,8 +1,10 @@
-import { DocPencilIcon } from "@navikt/aksel-icons";
+import { DocPencilIcon, PersonPencilIcon, TasklistSendIcon } from "@navikt/aksel-icons";
 import { Tabs } from "@navikt/ds-react";
 import { Outlet } from "react-router";
 
+import { Begrunnelse } from "~/components/begrunnelse/Begrunnelse";
 import { Behandling } from "~/components/behandling/Behandling";
+import { MeldingOmVedtak } from "~/components/melding-om-vedtak/MeldingOmVedtak";
 import { OppgaveHandlinger } from "~/components/oppgave-handlinger/OppgaveHandlinger";
 import { OppgaveInformasjon } from "~/components/oppgave-informasjon/OppgaveInformasjon";
 import styles from "~/route-styles/oppgave.module.css";
@@ -21,11 +23,31 @@ export default function Oppgave() {
                   label="Redigere opplysninger"
                   icon={<DocPencilIcon />}
                 />
+
+                <Tabs.Tab
+                  value="begrunnelse"
+                  label="Saksbehandlers begrunnelse"
+                  icon={<PersonPencilIcon />}
+                />
+
+                <Tabs.Tab
+                  value="melding-om-vedtak"
+                  label="Melding om vedtak"
+                  icon={<TasklistSendIcon />}
+                />
               </Tabs.List>
             </div>
 
             <Tabs.Panel value="behandling">
               <Behandling readOnly={true} />
+            </Tabs.Panel>
+
+            <Tabs.Panel value="begrunnelse">
+              <Begrunnelse readOnly={true} />
+            </Tabs.Panel>
+
+            <Tabs.Panel value="melding-om-vedtak">
+              <MeldingOmVedtak readOnly={true} />
             </Tabs.Panel>
           </Tabs>
         </div>
