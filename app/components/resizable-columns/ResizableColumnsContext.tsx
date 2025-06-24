73eab913862b6bd7856e05f5e5bd6d3302bb6f0d
Sindre Sægrov <sindre.segrov@nav.no>
2025-06-24 14:56:08 +0200

    Fjern kommentar

--- app/components/resizable-columns/ResizableColumnsContext.tsx
+++ app/components/resizable-columns/ResizableColumnsContext.tsx
@@ -1,6 +1,5 @@
 import { createContext, RefObject, useContext } from "react";
 
-// Context to share refs and state between parent and children
 export interface ResizableColumnsContextType {
   leftRef: RefObject<HTMLDivElement | null>;
   rightRef: RefObject<HTMLDivElement | null>;
