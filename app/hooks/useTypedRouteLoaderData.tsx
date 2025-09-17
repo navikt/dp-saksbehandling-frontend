33b0694a55db8399fd57b325070e8bf7094cd5fb
Kent Daleng <kent.daleng@nav.no>
2025-09-17 11:25:33 +0200

    fjern deprecata avhengigheter
    Co-authored-by: Sindre Sægrov <sindre.segrov@nav.no>

--- app/hooks/useTypedRouteLoaderData.tsx
+++ app/hooks/useTypedRouteLoaderData.tsx
@@ -5,6 +5,7 @@ import type { loader as behandlingLoader } from "~/routes/behandling.$behandling
 import type { loader as oppgaveLoader } from "~/routes/oppgave.$oppgaveId";
 import type { loader as dagpengerRettLoader } from "~/routes/oppgave.$oppgaveId.dagpenger-rett.$behandlingId";
 import type { loader as dagpengerRettBehandlingLoader } from "~/routes/oppgave.$oppgaveId.dagpenger-rett.$behandlingId.behandle";
+import type { loader as meldingOmVedtakLoader } from "~/routes/oppgave.$oppgaveId.dagpenger-rett.$behandlingId.melding-om-vedtak";
 import type { loader as klageLoader } from "~/routes/oppgave.$oppgaveId.klage.$behandlingId";
 import type { loader as personLoader } from "~/routes/person.$personUuid";
 
@@ -16,6 +17,7 @@ type Loaders = {
   "routes/oppgave.$oppgaveId.klage.$behandlingId": typeof klageLoader;
   "routes/oppgave.$oppgaveId.dagpenger-rett.$behandlingId": typeof dagpengerRettLoader;
   "routes/oppgave.$oppgaveId.dagpenger-rett.$behandlingId.behandle": typeof dagpengerRettBehandlingLoader;
+  "routes/oppgave.$oppgaveId.dagpenger-rett.$behandlingId.melding-om-vedtak": typeof meldingOmVedtakLoader;
 };
 
 export function useTypedRouteLoaderData<T extends keyof Loaders>(route: T) {
