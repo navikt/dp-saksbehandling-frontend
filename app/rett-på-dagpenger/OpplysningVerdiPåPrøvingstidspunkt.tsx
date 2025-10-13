c5261cfda3302acb5ad62f17473c5a4770e3ea38
Kent Daleng <kent.daleng@nav.no>
2025-10-13 10:58:33 +0200

    wip: rettighetsperiode-cards
    Co-authored-by: Sindre Sægrov <sindre.segrov@nav.no>

---
+++ app/rett-på-dagpenger/OpplysningVerdiPåPrøvingstidspunkt.tsx
@@ -0,0 +1,66 @@
+import { BodyShort } from "@navikt/ds-react";
+
+import { formaterOpplysningVerdi } from "~/components/vilkår-tidslinje/VilkårTidslinje";
+
+import { components } from "../../openapi/behandling-typer";
+
+export function OpplysningVerdiPåPrøvingstidspunkt(props: {
+  label: string;
+  opplysninger: components["schemas"]["OpplysningsgruppeV2"][];
+  opplysningTypeId: string;
+  prøvingsdato?: string;
+}) {
+  const verdier = hentVerdierForOpplysning(
+    props.opplysninger,
+    props.opplysningTypeId,
+    props.prøvingsdato,
+  );
+  if (!verdier) {
+    return;
+  }
+
+  return (
+    <div className="flex flex-col gap-1">
+      <BodyShort size={"small"} weight={"semibold"}>
+        {props.label}
+      </BodyShort>
+      <BodyShort size={"small"}>
+        {verdier?.map((verdi) => formaterOpplysningVerdi(verdi)).join(", ")}
+      </BodyShort>
+    </div>
+  );
+}
+
+function erPrøvingsdatoInnenforPeriode(
+  prøvingsdato: string,
+  fraOgMed?: string | null,
+  tilOgMed?: string | null,
+) {
+  if (fraOgMed && tilOgMed) {
+    return prøvingsdato >= fraOgMed && prøvingsdato <= tilOgMed;
+  }
+  if (fraOgMed && !tilOgMed) {
+    return prøvingsdato >= fraOgMed;
+  }
+  if (!fraOgMed && tilOgMed) {
+    return prøvingsdato <= tilOgMed;
+  }
+  return true;
+}
+
+function hentVerdierForOpplysning(
+  opplysninger: components["schemas"]["OpplysningsgruppeV2"][],
+  opplysningTypeId: string,
+  prøvingsdato?: string,
+): components["schemas"]["Opplysningsverdi"][] | undefined {
+  return opplysninger
+    .find((opplysning) => opplysning.opplysningTypeId === opplysningTypeId)
+    ?.perioder.filter((periode) =>
+      erPrøvingsdatoInnenforPeriode(
+        prøvingsdato ?? "",
+        periode.gyldigFraOgMed,
+        periode.gyldigTilOgMed,
+      ),
+    )
+    .map((periode) => periode.verdi);
+}
--- app/rett-på-dagpenger/RettPåDagpenger.tsx
+++ app/rett-på-dagpenger/RettPåDagpenger.tsx
@@ -6,17 +6,19 @@ import { useState } from "react";
 import { TidslinjeNavigering } from "~/components/tidslinje-navigering/TidslinjeNavigering";
 import {
   AntallUkerITidslinje,
-  formaterOpplysningVerdi,
   TidslinjeStartSlutt,
 } from "~/components/vilkår-tidslinje/VilkårTidslinje";
+import { formaterTilNorskDato } from "~/utils/dato.utils";
 
 import { components } from "../../openapi/behandling-typer";
+import { OpplysningVerdiForPeriode } from "./OpplysningVerdiForPeriode";
+import { OpplysningVerdiPåPrøvingstidspunkt } from "./OpplysningVerdiPåPrøvingstidspunkt";
 
 interface IProps {
   behandling: components["schemas"]["BehandlingsresultatV2"];
 }
 
-const opplysningeneViBryrOssOm = [
+const generelleOpplysninger = [
   { id: "0194881f-91d1-7df2-ba1d-4533f37fcc76", label: "Vurdert fra" },
   { id: "0194881f-9435-72a8-b1ce-9575cbc2a767", label: "Arbeidstid før tap" },
   { id: "0194881f-9444-7a73-a458-0af81c034d8b", label: "Rettighetstype" },
@@ -24,6 +26,14 @@ const opplysningeneViBryrOssOm = [
   { id: "0194881f-943f-78d9-b874-00a4944c54ef", label: "Egenandel" },
   { id: "0194881f-9410-7481-b263-4606fdd10cba", label: "Beregnet basert på" },
   { id: "0194881f-9410-7481-b263-4606fdd10cbd", label: "Inntektsgrunnlag" },
+];
+
+const rettighetsperiodeOpplysninger = [
+  { id: "0194881f-9444-7a73-a458-0af81c034d8b", label: "Rettighetstype" },
+  { id: "0194881f-9435-72a8-b1ce-9575cbc2a76b", label: "Ny arbeidstid per uke" },
+  { id: "0194881f-9434-79e8-a64d-1a23cc5d86eb", label: "Samordnet dagssats uten barnetillegg" },
+  { id: "0194881f-9428-74d5-b160-f63a4c61a24d", label: "Samordnet dagssats med barnetillegg" },
+  { id: "0194881f-9445-734c-a7ee-045edf29b523", label: "Godkjent unntak for utdanning" },
 ];
 
 export function RettPåDagpenger({ behandling }: IProps) {
@@ -96,11 +106,11 @@ export function RettPåDagpenger({ behandling }: IProps) {
       <div className={"card card-sunken p-4"}>
         <Heading size={"small"}>Generelt</Heading>
         <section className="grid grid-cols-4 gap-2">
-          {opplysningeneViBryrOssOm.map(({ id, label }) => (
+          {generelleOpplysninger.map(({ id, label }) => (
             <OpplysningVerdiPåPrøvingstidspunkt
               key={id}
               label={label}
-              behandling={behandling}
+              opplysninger={behandling.opplysninger}
               opplysningTypeId={id}
               prøvingsdato={prøvingsdato}
             />
@@ -116,59 +126,42 @@ export function RettPåDagpenger({ behandling }: IProps) {
           </div>
         </section>
       </div>
-    </div>
-  );
-}
-
-function erDatoInnenforPeriode(dato: string, fraOgMed?: string | null, tilOgMed?: string | null) {
-  if (fraOgMed && tilOgMed) {
-    return dato >= fraOgMed && dato <= tilOgMed;
-  }
-  if (fraOgMed && !tilOgMed) {
-    return dato >= fraOgMed;
-  }
-  if (!fraOgMed && tilOgMed) {
-    return dato <= tilOgMed;
-  }
-  return true;
-}
-
-function hentVerdierForOpplysning(
-  behandling: components["schemas"]["BehandlingsresultatV2"],
-  opplysningTypeId: string,
-  prøvingsdato?: string,
-): components["schemas"]["Opplysningsverdi"][] | undefined {
-  return behandling.opplysninger
-    .find((opplysning) => opplysning.opplysningTypeId === opplysningTypeId)
-    ?.perioder.filter((periode) =>
-      erDatoInnenforPeriode(prøvingsdato ?? "", periode.gyldigFraOgMed, periode.gyldigTilOgMed),
-    )
-    .map((periode) => periode.verdi);
-}
-
-function OpplysningVerdiPåPrøvingstidspunkt(props: {
-  label: string;
-  behandling: components["schemas"]["BehandlingsresultatV2"];
-  opplysningTypeId: string;
-  prøvingsdato?: string;
-}) {
-  const verdier = hentVerdierForOpplysning(
-    props.behandling,
-    props.opplysningTypeId,
-    props.prøvingsdato,
-  );
-  if (!verdier) {
-    return;
-  }
-
-  return (
-    <div className="flex flex-col gap-1">
-      <BodyShort size={"small"} weight={"semibold"}>
-        {props.label}
-      </BodyShort>
-      <BodyShort size={"small"}>
-        {verdier?.map((verdi) => formaterOpplysningVerdi(verdi)).join(", ")}
-      </BodyShort>
+      {behandling.rettighetsperioder.map((periode, index) => (
+        <div key={index} className={"card card-sunken p-4"}>
+          <Heading size={"small"}>Rettighetsperiode {index + 1}</Heading>
+          <section className="grid grid-cols-4 gap-2">
+            <div className="flex flex-col gap-1">
+              <BodyShort size={"small"} weight={"semibold"}>
+                Rett på dagpenger
+              </BodyShort>
+              <BodyShort size={"small"}>{periode.harRett ? "Ja" : "Nei"}</BodyShort>
+            </div>
+            <div className="flex flex-col gap-1">
+              <BodyShort size={"small"} weight={"semibold"}>
+                Fra og med
+              </BodyShort>
+              <BodyShort size={"small"}>{formaterTilNorskDato(periode.fraOgMed)}</BodyShort>
+            </div>
+            <div className="flex flex-col gap-1">
+              <BodyShort size={"small"} weight={"semibold"}>
+                Til og med
+              </BodyShort>
+              <BodyShort size={"small"}>
+                {periode.tilOgMed ? formaterTilNorskDato(periode.tilOgMed) : "--"}
+              </BodyShort>
+            </div>
+            {rettighetsperiodeOpplysninger.map(({ id, label }) => (
+              <OpplysningVerdiForPeriode
+                key={id}
+                label={label}
+                opplysninger={behandling.opplysninger}
+                opplysningTypeId={id}
+                rettighetsperiode={periode}
+              />
+            ))}
+          </section>
+        </div>
+      ))}
     </div>
   );
 }
