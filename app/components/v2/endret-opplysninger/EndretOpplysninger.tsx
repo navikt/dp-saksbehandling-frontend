c129833aa91fc04207cf2b1ba4f755830aac52f3
Sindre Sægrov <sindre.segrov@nav.no>
2025-11-11 13:39:10 +0100

    Legg til tekst hvis det ikke er noen vurderinger

--- app/components/v2/endret-opplysninger/EndretOpplysninger.tsx
+++ app/components/v2/endret-opplysninger/EndretOpplysninger.tsx
@@ -15,42 +15,48 @@ export function EndretOpplysninger({ vurderinger }: IProps) {
   return (
     <div className={"card p-4"}>
       <Heading size={"small"}>Saksbehandlers vurderinger</Heading>
+      {vurderinger.opplysninger.length === 0 && (
+        <BodyShort className={"pt-2"}>Ingen vurderinger</BodyShort>
+      )}
+      {vurderinger.opplysninger.length > 0 && (
+        <Accordion size={"small"} className={"accordion--subtil mt-2"}>
+          {vurderinger.opplysninger.map((opplysning) => (
+            <Accordion.Item key={opplysning.id} defaultOpen={underKontroll}>
+              <Accordion.Header>{opplysning.navn}</Accordion.Header>
+              <Accordion.Content>
+                <div className={styles.opplysningDetail}>
+                  <BodyShort size={"small"} weight={"semibold"}>
+                    Periode
+                  </BodyShort>
+                  <BodyShort size={"small"}>
+                    {opplysning.gyldigFraOgMed ? opplysning.gyldigFraOgMed : "--"} –{" "}
+                    {opplysning.gyldigTilOgMed ? opplysning.gyldigTilOgMed : "--"}
+                  </BodyShort>
+                </div>
 
-      <Accordion size={"small"} className={"accordion--subtil mt-2"}>
-        {vurderinger.opplysninger.map((opplysning) => (
-          <Accordion.Item key={opplysning.id} defaultOpen={underKontroll}>
-            <Accordion.Header>{opplysning.navn}</Accordion.Header>
-            <Accordion.Content>
-              <div className={styles.opplysningDetail}>
-                <BodyShort size={"small"} weight={"semibold"}>
-                  Periode
-                </BodyShort>
-                <BodyShort size={"small"}>
-                  {opplysning.gyldigFraOgMed ? opplysning.gyldigFraOgMed : "--"} –{" "}
-                  {opplysning.gyldigTilOgMed ? opplysning.gyldigTilOgMed : "--"}
-                </BodyShort>
-              </div>
+                <div className={styles.opplysningDetail}>
+                  <BodyShort size={"small"} weight={"semibold"}>
+                    Nytt svar
+                  </BodyShort>
+                  <BodyShort size={"small"}>
+                    {/* @ts-expect-error Fikset i v2 type*/}
+                    {formaterOpplysningVerdiV2(opplysning.verdien)}
+                  </BodyShort>
+                </div>
 
-              <div className={styles.opplysningDetail}>
-                <BodyShort size={"small"} weight={"semibold"}>
-                  Nytt svar
-                </BodyShort>
-                <BodyShort size={"small"}>
-                  {/* @ts-expect-error Fikset i v2 type*/}
-                  {formaterOpplysningVerdiV2(opplysning.verdien)}
-                </BodyShort>
-              </div>
-
-              <div className={styles.opplysningDetail}>
-                <BodyShort size={"small"} weight={"semibold"}>
-                  Begrunnelse
-                </BodyShort>
-                <BodyShort size={"small"}>{opplysning.kilde?.begrunnelse?.verdi ?? "--"}</BodyShort>
-              </div>
-            </Accordion.Content>
-          </Accordion.Item>
-        ))}
-      </Accordion>
+                <div className={styles.opplysningDetail}>
+                  <BodyShort size={"small"} weight={"semibold"}>
+                    Begrunnelse
+                  </BodyShort>
+                  <BodyShort size={"small"}>
+                    {opplysning.kilde?.begrunnelse?.verdi ?? "--"}
+                  </BodyShort>
+                </div>
+              </Accordion.Content>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      )}
     </div>
   );
 }
