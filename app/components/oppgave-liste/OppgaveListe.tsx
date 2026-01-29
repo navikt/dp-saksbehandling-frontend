5835be0c599a6882165c6797bd5c91a4e440bf7a
Sindre Sægrov <sindre.segrov@nav.no>
2026-01-29 13:01:34 +0100

    Wrap årsaker hvis det blir for langt

--- app/components/oppgave-liste/OppgaveListe.tsx
+++ app/components/oppgave-liste/OppgaveListe.tsx
@@ -175,7 +175,7 @@ export function OppgaveListe(props: IProps) {
                 </Table.DataCell>
 
                 <Table.DataCell>
-                  <div className={"flex gap-2"}>
+                  <div className={"flex flex-wrap gap-2"}>
                     {avslagsGrunner.map((emneknagg) => (
                       <Tag
                         key={emneknagg.visningsnavn}
