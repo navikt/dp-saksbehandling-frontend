94b92ea0be921b7cd66697f1a1ce59be77b68d75
Sindre Sægrov <sindre.segrov@nav.no>
2026-01-29 11:08:08 +0100

    Bruk riktig emneknagg kategori

--- app/components/oppgave-filter/OppgaveFilterAvbruttgrunner.tsx
+++ app/components/oppgave-filter/OppgaveFilterAvbruttgrunner.tsx
@@ -2,7 +2,7 @@ import { Checkbox, CheckboxGroup, Detail } from "@navikt/ds-react";
 
 import { useToggleSearchParam } from "~/hooks/useToggleSearchParam";
 
-const AVBRUTTGRUNNER = [
+const AVBRUTT_GRUNN = [
   "Behandles i Arena",
   "Flere søknader",
   "Trukket søknad",
@@ -11,19 +11,19 @@ const AVBRUTTGRUNNER = [
 
 export function OppgaveFilterAvbruttgrunner() {
   const { searchParams, toggleSearchParam } = useToggleSearchParam();
-  const emneknaggerParams = searchParams.getAll("AVBRUTTGRUNNER");
+  const emneknaggerParams = searchParams.getAll("AVBRUTT_GRUNN");
 
   return (
     <div>
       <Detail textColor="subtle">Avbruttgrunner</Detail>
       <CheckboxGroup legend="" size="small" className={"checkbox--compact"}>
-        {AVBRUTTGRUNNER.map((emneknagg) => (
+        {AVBRUTT_GRUNN.map((emneknagg) => (
           <Checkbox
             key={emneknagg}
-            name="AVBRUTTGRUNNER"
+            name="AVBRUTT_GRUNN"
             value={emneknagg}
             defaultChecked={emneknaggerParams.includes(emneknagg)}
-            onChange={(event) => toggleSearchParam("AVBRUTTGRUNNER", event.currentTarget.value)}
+            onChange={(event) => toggleSearchParam("AVBRUTT_GRUNN", event.currentTarget.value)}
           >
             {emneknagg}
           </Checkbox>
