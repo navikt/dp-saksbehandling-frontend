e78ea069ddd1fb37ede2d6932ff09694c89c4c0a
Sindre Sægrov <sindre.segrov@nav.no>
2026-02-09 15:35:12 +0100

    WIP Vis gjeldene vedtak på personoversikt

--- app/components/siste-sak/SisteSak.tsx
+++ app/components/siste-sak/SisteSak.tsx
@@ -2,14 +2,17 @@ import { FolderFileIcon } from "@navikt/aksel-icons";
 import { BodyShort, CopyButton, Heading } from "@navikt/ds-react";
 
 import { BehandlingListe } from "~/components/behandling-liste/BehandlingListe";
+import { GjeldendeVedtak } from "~/components/gjeldende-vedtak/GjeldendeVedtak";
 
+import { components as behandlingComponents } from "../../../openapi/behandling-typer";
 import { components } from "../../../openapi/saksbehandling-typer";
 
 interface IProps {
   sak: components["schemas"]["Sak"];
+  dagpengerRettBehandling?: behandlingComponents["schemas"]["Behandling"];
 }
 
-export function SisteSak({ sak }: IProps) {
+export function SisteSak({ sak, dagpengerRettBehandling }: IProps) {
   const idGrupper = sak.id.split("-");
   const sisteIdGruppe = idGrupper.pop();
   const forsteIdGruppe = idGrupper.join("-");
@@ -32,6 +35,10 @@ export function SisteSak({ sak }: IProps) {
         </BodyShort>
         <CopyButton copyText={sak.id} size={"small"} title={"kopier sakid"} />
       </div>
+
+      {dagpengerRettBehandling && (
+        <GjeldendeVedtak dagpengerRettBehandling={dagpengerRettBehandling} />
+      )}
 
       <BehandlingListe behandlinger={sak.behandlinger} />
     </div>
