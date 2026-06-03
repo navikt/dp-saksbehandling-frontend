5d753928b52a40f64993545760ccd45205473c86
Anders <anders.dahlin@nav.no>
2026-06-03 10:52:37 +0200

    error and loading handling

--- app/components/dokument-oversikt/DokumentOversikt.tsx
+++ app/components/dokument-oversikt/DokumentOversikt.tsx
@@ -1,18 +1,19 @@
 import { useQueries } from "@tanstack/react-query";
-import { Suspense } from "react";
 
 import { JournalpostQuery } from "@/graphql/generated/saf/graphql";
 import { CenteredLoader } from "~/components/centered-loader/CenteredLoader";
 import { JournalpostOversikt } from "~/components/dokument-oversikt/JournalpostOversikt";
 import { useOppgave } from "~/hooks/useOppgave";
 
+import { AsyncErrorMelding } from "../async-error-melding/AsyncErrorMelding";
+
 const fetchJournalpost = (journalpostId: string): Promise<JournalpostQuery["journalpost"]> =>
   fetch(`/api/journalpost/${journalpostId}`).then((res) => res.json());
 
 export function DokumentOversikt() {
   const { oppgave } = useOppgave();
-  console.log("oppgave", oppgave);
-  const { data } = useQueries({
+
+  const { data, error, pending } = useQueries({
     queries: oppgave.journalpostIder.map((journalpostId) => ({
       queryKey: ["journalpost", journalpostId],
       queryFn: () => fetchJournalpost(journalpostId),
@@ -21,13 +22,21 @@ export function DokumentOversikt() {
       return {
         data: results.map((result) => result.data),
         pending: results.some((result) => result.isPending),
+        error: results.find((result) => result.error)?.error,
       };
     },
   });
-  console.log("data", data);
+
   return (
-    <Suspense fallback={<CenteredLoader size={"large"} loadingText={"Henter dokumenter"} />}>
-      <JournalpostOversikt journalposterResponse={data} />
-    </Suspense>
+    <>
+      {pending && <CenteredLoader size={"large"} loadingText={"Henter dokumenter"} />}
+      {error && (
+        <AsyncErrorMelding
+          tittel={"En feil oppsto når vi skulle hente ut dokumentene 🤖"}
+          feilmelding={error.message}
+        />
+      )}
+      {data && <JournalpostOversikt journalposterResponse={data} />}
+    </>
   );
 }
